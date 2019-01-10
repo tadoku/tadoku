29d5b813f9382859ac74b0f19856647c61af0b58
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-01-10 13:26:24 +0900

    add echo as a router

--- app/server.go
+++ app/server.go
@@ -1,7 +1,15 @@
 package app
 
+import (
+	"net/http"
+	"sync"
+
+	"github.com/labstack/echo"
+)
+
 // Server is a dependency container for the api
 type Server interface {
+	Router() *echo.Echo
 }
 
 // NewServer instantiates a new api server
@@ -10,9 +18,26 @@ func NewServer() Server {
 }
 
 type server struct {
+	router struct {
+		result *echo.Echo
+		once   sync.Once
+	}
+}
+
+func (s *server) Router() *echo.Echo {
+	holder := &s.router
+	holder.once.Do(func() {
+		holder.result = echo.New()
+		holder.result.GET("/", func(c echo.Context) error {
+			return c.String(http.StatusOK, "Hello, World!")
+		})
+	})
+	return holder.result
 }
 
 // RunServer starts the actual API server
 func RunServer(s Server) error {
+	router := s.Router()
+	router.Logger.Fatal(router.Start(":1234"))
 	return nil
 }
--- go.mod
+++ go.mod
@@ -1,1 +1,13 @@
 module github.com/tadoku/api
+
+require (
+	github.com/google/pprof v0.0.0-20190109223431-e84dfd68c163 // indirect
+	github.com/labstack/echo v3.3.5+incompatible
+	github.com/labstack/gommon v0.2.8 // indirect
+	github.com/mattn/go-colorable v0.0.9 // indirect
+	github.com/mattn/go-isatty v0.0.4 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v0.0.0-20170224212429-dcecefd839c4 // indirect
+	golang.org/x/arch v0.0.0-20181203225421-5a4828bb7045 // indirect
+	golang.org/x/crypto v0.0.0-20190103213133-ff983b9c42bc // indirect
+)

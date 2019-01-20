3ebb3061f34ca39de9da94f530192285d68d950e
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-01-21 00:27:28 +0900

    add testing config

---
+++ test/helpers.go
@@ -0,0 +1,24 @@
+package test
+
+import (
+	"github.com/creasty/configo"
+	"github.com/srvc/fail"
+)
+
+// Config contains testing configuration data
+type Config struct {
+	DatabaseURL          string `envconfig:"database_url" valid:"required"`
+	DatabaseMaxIdleConns int    `envconfig:"database_max_idle_conns" valid:"required"`
+	DatabaseMaxOpenConns int    `envconfig:"database_max_open_conns" valid:"required"`
+}
+
+// LoadConfig helper
+func LoadConfig() (*Config, error) {
+	c := &Config{}
+	opts := configo.Option{
+		Prefix: "testing",
+	}
+	err := configo.Load(c, opts)
+
+	return c, fail.Wrap(err)
+}

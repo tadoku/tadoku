a1f15278d5eb351fc491169149566a2babfa11cb
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-01-21 00:29:46 +0900

    add database sandboxing for testing

--- go.mod
+++ go.mod
@@ -1,6 +1,7 @@
 module github.com/tadoku/api
 
 require (
+	github.com/DATA-DOG/go-txdb v0.1.0
 	github.com/DavidHuie/gomigrate v0.0.0-20160809001028-4004e6142040
 	github.com/asaskevich/govalidator v0.0.0-20180720115003-f9ffefc3facf // indirect
 	github.com/creasty/configo v0.0.0-20170711121124-288b7e8980d2
---
+++ interfaces/repositories/prepare_test.go
@@ -0,0 +1,60 @@
+package repositories_test
+
+import (
+	"fmt"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/jmoiron/sqlx"
+	"github.com/tadoku/api/infra"
+	"github.com/tadoku/api/interfaces/rdb"
+	"github.com/tadoku/api/test"
+
+	txdb "github.com/DATA-DOG/go-txdb"
+	"github.com/DavidHuie/gomigrate"
+	_ "github.com/lib/pq"
+)
+
+func TestMain(m *testing.M) {
+	cfg := loadConfig()
+
+	// Must be called pgx so the sqlx mapper uses the correct notation
+	txdb.Register("pgx", "postgres", cfg.DatabaseURL)
+
+	code := m.Run()
+	defer os.Exit(code)
+}
+
+func loadConfig() *test.Config {
+	c, err := test.LoadConfig()
+	if err != nil {
+		panic("could not load config")
+	}
+
+	return c
+}
+
+func setupTestingSuite(t *testing.T) (rdb.SQLHandler, func() error) {
+	db, cleanup := prepareDB(t)
+
+	migrator, _ := gomigrate.NewMigrator(db.DB, gomigrate.Postgres{}, "./../../migrations")
+
+	err := migrator.Migrate()
+	if err != nil {
+		t.Fatalf("could not migrate testing DB: %s", err)
+	}
+
+	return infra.NewSQLHandler(db), cleanup
+}
+
+func prepareDB(t *testing.T) (db *sqlx.DB, cleanup func() error) {
+	cName := fmt.Sprintf("connection_%d", time.Now().UnixNano())
+	db, err := sqlx.Open("pgx", cName)
+
+	if err != nil {
+		t.Fatalf("open pgx connection: %s", err)
+	}
+
+	return db, db.Close
+}

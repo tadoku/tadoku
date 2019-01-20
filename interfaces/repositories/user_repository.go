0e6ce6f813d75bcd6b9910f1192177733c999b38
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-01-20 19:41:42 +0900

    implement UserRepository.Store

--- interfaces/repositories/user_repository.go
+++ interfaces/repositories/user_repository.go
@@ -1,11 +1,13 @@
 package repositories
 
 import (
+	"github.com/srvc/fail"
 	"github.com/tadoku/api/domain"
 	"github.com/tadoku/api/interfaces/rdb"
 	"github.com/tadoku/api/usecases"
 )
 
+// NewUserRepository instantiates a new user repository
 func NewUserRepository(sqlHandler rdb.SQLHandler) usecases.UserRepository {
 	return &userRepository{sqlHandler: sqlHandler}
 }
@@ -15,9 +17,35 @@ type userRepository struct {
 }
 
 func (r *userRepository) Store(user domain.User) error {
-	return nil
+	if user.ID == 0 {
+		return r.create(user)
+	}
+
+	return r.update(user)
+}
+
+func (r *userRepository) create(user domain.User) error {
+	query := `
+		insert into users
+		(email, display_name, password, role, preferences)
+		values (:email, :display_name, :password, :role, :preferences)
+	`
+	_, err := r.sqlHandler.NamedExecute(query, user)
+	return fail.Wrap(err)
+}
+
+func (r *userRepository) update(user domain.User) error {
+	query := `
+		update users
+		set
+			display_name = :display_name,
+			preferences = :preferences
+		where id = :id
+	`
+	_, err := r.sqlHandler.NamedExecute(query, user)
+	return fail.Wrap(err)
 }
 
 func (r *userRepository) FindByID(id int) domain.User {
-	return nil
+	return domain.User{}
 }

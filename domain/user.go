14b5ba2c8e1c9bfe079b3db55212d4a0a1e40e54
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-01-21 21:24:35 +0900

    add password hashed boolean state

--- domain/user.go
+++ domain/user.go
@@ -2,12 +2,13 @@ package domain
 
 // User holds everything related to a user's account data
 type User struct {
-	ID          uint64       `json:"id" db:"id"`
-	Email       string       `json:"email" db:"email"`
-	DisplayName string       `json:"display_name" db:"display_name"`
-	Password    string       `json:"password" db:"password"`
-	Role        Role         `json:"role" db:"role"`
-	Preferences *Preferences `json:"preferences" db:"preferences"`
+	ID               uint64       `json:"id" db:"id"`
+	Email            string       `json:"email" db:"email"`
+	DisplayName      string       `json:"display_name" db:"display_name"`
+	Password         string       `json:"password" db:"password"`
+	Role             Role         `json:"role" db:"role"`
+	Preferences      *Preferences `json:"preferences" db:"preferences"`
+	isPasswordHashed bool
 }
 
 // Users is a collection of users

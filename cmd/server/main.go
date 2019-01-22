91b45470ec2d220fadd545cc382bfc734d2ab863
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-01-22 22:32:02 +0900

    make sure err from auto configure is checked

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/tadoku/api/app"
 )
 
 func main() {
 	deps := app.NewServerDependencies()
-	deps.AutoConfigure()
+	err := deps.AutoConfigure()
+	if err != nil {
+		panic(fmt.Sprintf("Server cannot be started: %v\n", err))
+	}
+
 	app.RunServer(deps)
 }

5b5bb75e20f01ab3a46e243f0cd6e4b3645423e1
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-01-22 21:22:33 +0900

    make sure password can not be exported into json

--- domain/user.go
+++ domain/user.go
@@ -1,11 +1,13 @@
 package domain
 
+type password string
+
 // User holds everything related to a user's account data
 type User struct {
 	ID               uint64       `json:"id" db:"id"`
 	Email            string       `json:"email" db:"email"`
 	DisplayName      string       `json:"display_name" db:"display_name"`
-	Password         string       `json:"password" db:"password"`
+	Password         password     `json:"password" db:"password"`
 	Role             Role         `json:"role" db:"role"`
 	Preferences      *Preferences `json:"preferences" db:"preferences"`
 	isPasswordHashed bool
@@ -17,4 +19,9 @@ type Users []User
 // NeedsHashing tells you if the password is in need of being hashed
 func (u *User) NeedsHashing() bool {
 	return u.Password != "" && !u.isPasswordHashed
+}
+
+// Prevent the password from being exported into something client-facing
+func (password) MarshalJSON() ([]byte, error) {
+	return []byte(`""`), nil
 }

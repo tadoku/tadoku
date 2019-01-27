45543776916b37a4c909c52ebbbbca4bd53513e1
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-01-27 18:33:52 +0900

    make health service test a blackbox test

--- interfaces/services/health_service_test.go
+++ interfaces/services/health_service_test.go
@@ -1,20 +1,21 @@
-package services
+package services_test
 
 import (
 	"testing"
 
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
+	"github.com/tadoku/api/interfaces/services"
 )
 
 func TestHealthService_Ping(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 
-	ctx := NewMockContext(ctrl)
+	ctx := services.NewMockContext(ctrl)
 	ctx.EXPECT().String(200, "pong")
 
-	s := NewHealthService()
+	s := services.NewHealthService()
 	err := s.Ping(ctx)
 
 	assert.NoError(t, err)

591c2d00c5d5c633298ef493246bd8a47f8482a2
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-03-02 16:23:26 +0900

    add validation annotations

--- domain/contest.go
+++ domain/contest.go
@@ -5,9 +5,9 @@ import "time"
 // Contest contains the data about when a contest is being held
 type Contest struct {
 	ID    uint64    `json:"id" db:"id"`
-	Start time.Time `json:"start" db:"start"`
-	End   time.Time `json:"end" db:"end"`
-	Open  bool
+	Start time.Time `json:"start" db:"start" valid:"required"`
+	End   time.Time `json:"end" db:"end" valid:"required"`
+	Open  bool      `json:"open" db:"open" valid:"required"`
 }
 
 // Contests is a collection of contests

d40380bbab41abb3fa0ebc02de5c2357c39f0cb1
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-04-29 21:26:17 +0900

    remove mediums from database

--- migrations/201901281844_contest_down.sql
+++ migrations/201901281844_contest_down.sql
@@ -1,8 +1,6 @@
 drop table contests cascade;
 drop table contest_logs cascade;
-drop table mediums cascade;
 drop table languages cascade;
 
 drop sequence if exists contest_seq;
-drop sequence if exists medium_seq;
 drop sequence if exists contest_log_seq;
--- migrations/201901281844_contest_up.sql
+++ migrations/201901281844_contest_up.sql
@@ -1,19 +1,11 @@
 create sequence contest_seq;
 create sequence contest_log_seq;
-create sequence medium_seq;
 
 create table contests (
   id bigint check (id > 0) not null default nextval ('contest_seq'),
   start date not null,
   "end" date not null,
   open boolean not null default false,
-  primary key (id)
-);
-
-create table mediums (
-  id smallint check (id > 0) not null default nextval ('medium_seq'),
-  description text not null,
-  points float(2) not null,
   primary key (id)
 );
 
@@ -39,8 +31,6 @@ create table contest_logs (
 create index content_logs_contest_id on contest_logs(contest_id);
 create index content_logs_user_id on contest_logs(user_id);
 create index content_logs_language_code on contest_logs(language_code);
-create index content_logs_medium_id on contest_logs(medium_id);
 
 alter sequence contest_seq restart with 1;
-alter sequence medium_seq restart with 1;
 alter sequence contest_log_seq restart with 1;

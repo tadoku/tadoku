d82b818d050eed23ce3e3567d065579e1524c7a2
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-04-30 23:33:29 +0900

    integrate service in the app

--- app/services.go
+++ app/services.go
@@ -6,18 +6,20 @@ import (
 
 // Services is a collection of all services
 type Services struct {
-	Health  services.HealthService
-	Session services.SessionService
-	Contest services.ContestService
-	Ranking services.RankingService
+	Health     services.HealthService
+	Session    services.SessionService
+	Contest    services.ContestService
+	Ranking    services.RankingService
+	ContestLog services.ContestLogService
 }
 
 // NewServices initializes all interactors
 func NewServices(i *Interactors) *Services {
 	return &Services{
-		Health:  services.NewHealthService(),
-		Session: services.NewSessionService(i.Session),
-		Contest: services.NewContestService(i.Contest),
-		Ranking: services.NewRankingService(i.Ranking),
+		Health:     services.NewHealthService(),
+		Session:    services.NewSessionService(i.Session),
+		Contest:    services.NewContestService(i.Contest),
+		Ranking:    services.NewRankingService(i.Ranking),
+		ContestLog: services.NewContestLogService(i.ContestLog),
 	}
 }

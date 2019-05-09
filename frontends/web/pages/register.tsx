b810291c0bbab5fb3d7e9b817a322d28f3aa6a4a
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-05-09 21:30:58 +0900

    add placeholder pages

---
+++ frontends/web/pages/register.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/ui/Layout'
+
+const Register = () => {
+  return (
+    <Layout>
+      <h2>Create a new account</h2>
+    </Layout>
+  )
+}
+
+export default Register
---
+++ frontends/web/pages/sign-in.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/ui/Layout'
+
+const SignIn = () => {
+  return (
+    <Layout>
+      <h2>Sign in</h2>
+    </Layout>
+  )
+}
+
+export default SignIn

164b865a73973f1d6bedcb22dd4fb2f8fb06941c
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-05-09 22:22:05 +0900

    add sign in form

--- frontends/web/pages/sign-in.tsx
+++ frontends/web/pages/sign-in.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Layout from '../components/ui/Layout'
+import SignInForm from '../components/forms/SignIn'
 
 const SignIn = () => {
   return (
     <Layout>
       <h2>Sign in</h2>
+      <SignInForm />
     </Layout>
   )
 }

494f40362430bc6d8836472b51340a6f727d639a
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-05-09 23:13:28 +0900

    add api for sign in

---
+++ frontends/web/domain/Api.ts
@@ -0,0 +1,46 @@
+import { User, RawUser } from './user'
+import 'isomorphic-fetch'
+
+const endpoint = 'http://localhost:4000'
+
+const post = (endpoint: string, body: any) =>
+  fetch(endpoint, {
+    method: 'post',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  })
+
+interface SignInResponse {
+  token: string
+  user: User
+}
+
+interface rawSignInResponse {
+  token: string
+  user: RawUser
+}
+
+export const SignIn = async (payload: {
+  email: string
+  password: string
+}): Promise<SignInResponse | undefined> => {
+  const response = await post(`${endpoint}/login`, payload)
+
+  if (response.status !== 200) {
+    return undefined
+  }
+
+  const data: rawSignInResponse = await response.json()
+
+  return {
+    token: data.token,
+    user: {
+      id: data.user.id,
+      email: data.user.email,
+      displayName: data.user.display_name,
+    },
+  }
+}
---
+++ frontends/web/domain/User.ts
@@ -0,0 +1,11 @@
+export interface User {
+  id: number
+  email: string
+  displayName: string
+}
+
+export interface RawUser {
+  id: number
+  email: string
+  display_name: string
+}

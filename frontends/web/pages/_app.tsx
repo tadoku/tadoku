9346365217e8ba0b4cacc5b5ca2331567ce526e9
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-05-10 14:03:44 +0900

    restore user state on page load

--- frontends/web/pages/_app.tsx
+++ frontends/web/pages/_app.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import { initializeStore } from './../store'
+import { initializeStore, SessionActionTypes } from './../store'
 import { Provider } from 'react-redux'
 import App, { Container, NextAppContext } from 'next/app'
 import withRedux from 'next-redux-wrapper'
 import { Store } from 'redux'
+import { loadUser } from '../domain/Session'
 
 class MyApp extends App<{ store: Store }> {
   static async getInitialProps({ Component, ctx }: NextAppContext) {
@@ -12,6 +13,17 @@ class MyApp extends App<{ store: Store }> {
       : {}
 
     return { pageProps }
+  }
+
+  componentDidMount() {
+    const payload = loadUser()
+
+    if (payload) {
+      this.props.store.dispatch({
+        type: SessionActionTypes.SessionSignIn,
+        payload,
+      })
+    }
   }
 
   render() {

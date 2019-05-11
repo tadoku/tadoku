8c89f7e5e52f4dedd6c576a51f1344cc6da656a8
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-05-11 22:09:26 +0900

    add api get method

--- frontends/web/domain/api/api.ts
+++ frontends/web/domain/api/api.ts
@@ -2,6 +2,15 @@ import 'isomorphic-fetch'
 
 const root = 'http://localhost:4000'
 
+export const get = (endpoint: string) =>
+  fetch(`${root}${endpoint}`, {
+    method: 'get',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  })
+
 export const post = (endpoint: string, body: any) =>
   fetch(`${root}${endpoint}`, {
     method: 'post',

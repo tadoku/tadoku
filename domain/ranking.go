58e78c3f9f862521a1e7609085ba6358bedba29a
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-05-12 14:04:35 +0900

    add view concept to rankings

--- domain/ranking.go
+++ domain/ranking.go
@@ -18,5 +18,34 @@ type Ranking struct {
 	UserDisplayName string `json:"user_display_name" db:"user_display_name"`
 }
 
+// GetView gets the external view representation of a Ranking
+func (r Ranking) GetView() RankingView {
+	return RankingView{
+		UserID:          r.UserID,
+		UserDisplayName: r.UserDisplayName,
+		Language:        r.Language,
+		Amount:          r.Amount,
+	}
+}
+
 // Rankings is a collection of Ranking
 type Rankings []Ranking
+
+// GetView gets the external view representation of a Rankings collection
+func (r Rankings) GetView() []RankingView {
+	result := make([]RankingView, len(r))
+
+	for i, val := range r {
+		result[i] = val.GetView()
+	}
+
+	return result
+}
+
+// RankingView is a representation of a ranking for external usages
+type RankingView struct {
+	UserID          uint64       `json:"user_id"`
+	UserDisplayName string       `json:"user_display_name"`
+	Language        LanguageCode `json:"language_code"`
+	Amount          float32      `json:"amount"`
+}

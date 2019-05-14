5138a2acd955f0cfd35af87b6a8e9d8f2659fe27
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-05-14 23:25:03 +0900

    refactor session side effects into a component

---
+++ frontends/web/app/session/components/SessionEffects.tsx
@@ -0,0 +1,29 @@
+import { useEffect } from 'react'
+import { Dispatch } from 'redux'
+import * as SessionStore from '../redux'
+import { loadUserFromLocalStorage } from '../storage'
+import { connect } from 'react-redux'
+
+const SessionEffects = ({ loadUser }: { loadUser: () => void }) => {
+  useEffect(() => loadUser(), [])
+
+  return null
+}
+
+const mapDispatchToProps = (dispatch: Dispatch<SessionStore.Action>) => ({
+  loadUser: () => {
+    const payload = loadUserFromLocalStorage()
+
+    if (payload) {
+      dispatch({
+        type: SessionStore.ActionTypes.SessionSignIn,
+        payload,
+      })
+    }
+  },
+})
+
+export default connect(
+  null,
+  mapDispatchToProps,
+)(SessionEffects)

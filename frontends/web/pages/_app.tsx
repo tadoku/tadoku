06eacbd808113fe3aa9233f8fcd9eb26cbfcda3e
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-05-14 23:45:33 +0900

    rename effects component file

--- frontends/web/pages/_app.tsx
+++ frontends/web/pages/_app.tsx
@@ -6,8 +6,8 @@ import withRedux from 'next-redux-wrapper'
 import { Store } from 'redux'
 import { loadUserFromLocalStorage } from '../app/session/storage'
 import * as SessionStore from '../app/session/redux'
-import SessionEffects from '../app/session/components/SessionEffects'
-import RankingEffects from '../app/ranking/components/RankingEffects'
+import SessionEffects from '../app/session/components/Effects'
+import RankingEffects from '../app/ranking/components/Effects'
 
 class MyApp extends App<{ store: Store }> {
   static async getInitialProps({ Component, ctx }: NextAppContext) {

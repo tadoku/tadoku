3da1dbf5a7fafd0a2a395ac51ab1187382a3c282
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-05-18 17:28:10 +0900

    start id from 1 for medium ids

--- frontends/web/app/ranking/database.ts
+++ frontends/web/app/ranking/database.ts
@@ -1,15 +1,15 @@
 import { Medium, Language } from './interfaces'
 
 export const AllMediums: Medium[] = [
-  { id: 0, description: 'Book' },
-  { id: 1, description: 'Manga' },
-  { id: 2, description: 'Net' },
-  { id: 3, description: 'Full game' },
-  { id: 4, description: 'Game' },
-  { id: 5, description: 'Lyric' },
-  { id: 6, description: 'Subs' },
-  { id: 7, description: 'News' },
-  { id: 8, description: 'Sentences' },
+  { id: 1, description: 'Book' },
+  { id: 2, description: 'Manga' },
+  { id: 3, description: 'Net' },
+  { id: 4, description: 'Full game' },
+  { id: 5, description: 'Game' },
+  { id: 6, description: 'Lyric' },
+  { id: 7, description: 'Subs' },
+  { id: 8, description: 'News' },
+  { id: 9, description: 'Sentences' },
 ]
 
 export const AllLanguages: Language[] = [

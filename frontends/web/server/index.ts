70ea429a9e0847b58786dfb57571f120f91e2a46
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-05-18 22:06:21 +0900

    add server in ts

---
+++ frontends/web/server/index.ts
@@ -0,0 +1,28 @@
+import * as next from 'next'
+import * as Express from 'express'
+
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+app
+  .prepare()
+  .then(() => {
+    const server = Express()
+
+    server.get('*', (req, res) => {
+      return handle(req, res)
+    })
+
+    server.listen(3000, (err: Error) => {
+      if (err) {
+        throw err
+      }
+
+      console.log('> Ready and accepting requests')
+    })
+  })
+  .catch(ex => {
+    console.error(ex.stack)
+    process.exit(1)
+  })

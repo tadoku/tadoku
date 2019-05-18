400a84e3d6c6ea1389c7242e54747e2844b72544
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-05-18 13:23:18 +0900

    add languages to hardcoded database

--- frontends/web/app/ranking/database.ts
+++ frontends/web/app/ranking/database.ts
@@ -1,4 +1,4 @@
-import { Medium } from './interfaces'
+import { Medium, Language } from './interfaces'
 
 export const AllMediums: Medium[] = [
   { id: 0, description: 'Book' },
@@ -11,3 +11,31 @@ export const AllMediums: Medium[] = [
   { id: 7, description: 'News' },
   { id: 8, description: 'Sentences' },
 ]
+
+export const AllLanguages: Language[] = [
+  { code: 'zho', name: 'Chinese' },
+  { code: 'nld', name: 'Dutch' },
+  { code: 'eng', name: 'English' },
+  { code: 'fra', name: 'French' },
+  { code: 'deu', name: 'German' },
+  { code: 'ell', name: 'Greek' },
+  { code: 'gle', name: 'Irish' },
+  { code: 'ita', name: 'Italian' },
+  { code: 'jpn', name: 'Japanese' },
+  { code: 'kor', name: 'Korean' },
+  { code: 'por', name: 'Portuguese' },
+  { code: 'rus', name: 'Russian' },
+  { code: 'spa', name: 'Spanish' },
+  { code: 'swe', name: 'Swedish' },
+  { code: 'tur', name: 'Turkish' },
+]
+
+export const LanguageByCode: { [key: string]: Language } = AllLanguages.reduce(
+  (previous, current) => {
+    return {
+      ...previous,
+      [current.code]: current,
+    }
+  },
+  {},
+)

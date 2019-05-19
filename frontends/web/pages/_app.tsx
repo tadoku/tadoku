2e3a54b33531c42877f27ce669f16dc57f8e3f8d
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-05-19 23:29:10 +0900

    add react-vis

--- frontends/web/pages/_app.tsx
+++ frontends/web/pages/_app.tsx
@@ -7,6 +7,7 @@ import { Store } from 'redux'
 import { loadUserFromLocalStorage } from '../app/session/storage'
 import * as SessionStore from '../app/session/redux'
 import AppEffects from '../app/AppEffects'
+import 'react-vis/dist/style.css'
 
 class MyApp extends App<{ store: Store }> {
   static async getInitialProps({ Component, ctx }: NextAppContext) {

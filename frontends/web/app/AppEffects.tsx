1526f745bd4e05643e414a6e5f21db9da24c5dc2
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-05-19 12:26:47 +0900

    refactor effects into app effects

---
+++ frontends/web/app/AppEffects.tsx
@@ -0,0 +1,13 @@
+import SessionEffects from '../app/session/components/Effects'
+import RankingEffects from '../app/ranking/components/Effects'
+import ContestEffects from '../app/contest/components/Effects'
+
+const AppEffects = () => (
+  <>
+    <SessionEffects />
+    <RankingEffects />
+    <ContestEffects />
+  </>
+)
+
+export default AppEffects

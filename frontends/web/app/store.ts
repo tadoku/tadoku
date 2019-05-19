e1f4d6b43d97b12eee6e0ac5e89fe7f69008e034
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-05-19 12:46:07 +0900

    add contest reducer to store

--- frontends/web/app/store.ts
+++ frontends/web/app/store.ts
@@ -1,23 +1,30 @@
 import { createStore, combineReducers } from 'redux'
-import * as SessionStore from './session/redux'
+import * as ContestStore from './contest/redux'
 import * as RankingStore from './ranking/redux'
+import * as SessionStore from './session/redux'
 
 const initialState = {
-  session: SessionStore.initialState,
+  contest: ContestStore.initialState,
   ranking: RankingStore.initialState,
+  session: SessionStore.initialState,
 }
 
 export type State = typeof initialState
-export type Action = SessionStore.Action | RankingStore.Action
+export type Action =
+  | ContestStore.Action
+  | RankingStore.Action
+  | SessionStore.Action
 
 export const actionTypes = {
-  ...SessionStore.ActionTypes,
+  ...ContestStore.ActionTypes,
   ...RankingStore.ActionTypes,
+  ...SessionStore.ActionTypes,
 }
 
 export const reducer = combineReducers({
-  session: SessionStore.reducer,
+  contest: ContestStore.reducer,
   ranking: RankingStore.reducer,
+  session: SessionStore.reducer,
 })
 
 export function initializeStore(state = initialState) {

23b61d1986b040ef775c1b8245b0bb88cf4f741a
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-05-25 16:50:39 +0900

    add description field to contest

--- domain/contest.go
+++ domain/contest.go
@@ -8,10 +8,11 @@ import (
 
 // Contest contains the data about when a contest is being held
 type Contest struct {
-	ID    uint64    `json:"id" db:"id"`
-	Start time.Time `json:"start" db:"start" valid:"required"`
-	End   time.Time `json:"end" db:"end" valid:"required"`
-	Open  bool      `json:"open" db:"open"`
+	ID          uint64    `json:"id" db:"id"`
+	Description string    `json:"description" db:"description", valid:"required"`
+	Start       time.Time `json:"start" db:"start" valid:"required"`
+	End         time.Time `json:"end" db:"end" valid:"required"`
+	Open        bool      `json:"open" db:"open"`
 }
 
 // Contests is a collection of contests

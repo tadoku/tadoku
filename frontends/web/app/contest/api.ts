ba469a657bf124ec5953bc8ba538af85dd816c99
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-05-26 17:11:11 +0900

    add api endpoint to get contest

--- frontends/web/app/contest/api.ts
+++ frontends/web/app/contest/api.ts
@@ -1,6 +1,23 @@
 import { get } from '../api'
 import { Contest, rawContest } from './interfaces'
 
+const getContest = async (contestId: number): Promise<Contest | undefined> => {
+  const response = await get(`/contests/${contestId}`)
+
+  if (response.status !== 200) {
+    return undefined
+  }
+
+  const data: rawContest = await response.json()
+
+  return {
+    id: data.id,
+    start: new Date(data.start),
+    end: new Date(data.end),
+    open: data.open,
+  }
+}
+
 const getLatest = async (): Promise<Contest | undefined> => {
   const response = await get(`/contests/latest`)
 
@@ -19,6 +36,7 @@ const getLatest = async (): Promise<Contest | undefined> => {
 }
 
 const ContestApi = {
+  get: getContest,
   getLatest,
 }
 

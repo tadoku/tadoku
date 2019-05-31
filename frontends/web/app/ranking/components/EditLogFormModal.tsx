3e334782dccb2175fcd46c718def48bfb8e924b2
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-05-31 17:12:33 +0900

    style modal

--- frontends/web/app/ranking/components/EditLogFormModal.tsx
+++ frontends/web/app/ranking/components/EditLogFormModal.tsx
@@ -17,6 +17,22 @@ const EditLogFormModal = ({
       isOpen={!!log}
       onRequestClose={() => setLog(undefined)}
       contentLabel="Update"
+      style={{
+        content: {
+          top: '50%',
+          left: '50%',
+          right: 'auto',
+          bottom: 'auto',
+          marginRight: '-50%',
+          transform: 'translate(-50%, -50%)',
+          border: 0,
+          boxShadow: '4px 15px 20px 1px rgba(0, 0, 0, 0.28)',
+          padding: '40px',
+        },
+        overlay: {
+          backgroundColor: 'rgba(0, 0, 0, 0.4)',
+        },
+      }}
     >
       {log && <EditLogForm log={log} onSuccess={onSuccess} />}
     </Modal>

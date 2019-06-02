3b6006a1b210ed36048c99420b3eab2dbb20da11
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-06-02 14:00:53 +0900

    use standard modal for new log form modal

--- frontends/web/app/ranking/components/NewLogFormModal.tsx
+++ frontends/web/app/ranking/components/NewLogFormModal.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Modal from 'react-modal'
+import Modal from '../../ui/components/Modal'
 import EditLogForm from './EditLogForm'
 
 const NewLogFormModal = ({
@@ -16,22 +16,6 @@ const NewLogFormModal = ({
       isOpen={isOpen}
       onRequestClose={onCancel}
       contentLabel="Submit new pages"
-      style={{
-        content: {
-          top: '50%',
-          left: '50%',
-          right: 'auto',
-          bottom: 'auto',
-          marginRight: '-50%',
-          transform: 'translate(-50%, -50%)',
-          border: 0,
-          boxShadow: '4px 15px 20px 1px rgba(0, 0, 0, 0.28)',
-          padding: '40px',
-        },
-        overlay: {
-          backgroundColor: 'rgba(0, 0, 0, 0.4)',
-        },
-      }}
     >
       <EditLogForm onSuccess={onSuccess} onCancel={onCancel} />
     </Modal>

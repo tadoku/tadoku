50090044815a03e5178f057944c0ab2f71bbbef3
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-06-02 16:54:22 +0900

    convert react modal styling to styled component

--- frontends/web/app/ui/components/Modal.tsx
+++ frontends/web/app/ui/components/Modal.tsx
@@ -2,39 +2,51 @@ import React, { SFC } from 'react'
 import ReactModal from 'react-modal'
 import styled from 'styled-components'
 
-const modalStyles = {
-  content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
-    border: 0,
-    boxShadow: '4px 15px 20px 1px rgba(0, 0, 0, 0.28)',
-    padding: '40px',
-    minWidth: '200px',
-    maxWidth: '100vh',
-    maxHeight: 'calc(100vh + 4px)',
-    boxSizing: 'border-box',
-    borderRadius: '4px',
-  },
-  overlay: {
-    backgroundColor: 'rgba(0, 0, 0, 0.4)',
-  },
-}
-
 const ModalHeading = styled.h2`
   margin-top: 0;
   text-transform: uppercase;
   text-align: center;
 `
 
-const Modal: SFC<ReactModal.Props> = ({ children, style, ...props }) => (
-  <ReactModal style={style ? style : modalStyles} {...props}>
+const StyledModal = styled(ReactModal)`
+  // positioning
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  right: auto;
+  bottom: auto;
+  margin-right: -50%;
+  transform: translate(-50%, -50%);
+
+  // responsiveness
+  overflow: auto;
+  min-width: 200px;
+  max-width: 100vh;
+  max-height: calc(100vh + 4px);
+  box-sizing: border-box;
+  -webkit-overflow-scrolling: touch;
+
+  // styling
+  background: #fff;
+  outline: none;
+  border: 0;
+  box-shadow: 4px 15px 20px 1px rgba(0, 0, 0, 0.28);
+  padding: 40px;
+  border-radius: 4px;
+`
+
+const Modal: SFC<ReactModal.Props> = ({ children, ...props }) => (
+  <StyledModal
+    style={{
+      overlay: {
+        backgroundColor: 'rgba(0, 0, 0, 0.4)',
+      },
+    }}
+    {...props}
+  >
     {props.contentLabel && <ModalHeading>{props.contentLabel}</ModalHeading>}
     {children}
-  </ReactModal>
+  </StyledModal>
 )
 
 export default Modal

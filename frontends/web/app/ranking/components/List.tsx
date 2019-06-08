9977902225287555af90d45e1f351ba824b139ab
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-06-08 16:24:13 +0900

    improve styling of ranking list

--- frontends/web/app/ranking/components/List.tsx
+++ frontends/web/app/ranking/components/List.tsx
@@ -3,6 +3,35 @@ import Link from 'next/link'
 import { Ranking } from '../interfaces'
 import styled from 'styled-components'
 import { amountToPages } from '../transform'
+import Constants from '../../ui/Constants'
+
+interface Props {
+  rankings: Ranking[]
+}
+
+const RankingList = (props: Props) => (
+  <List>
+    {props.rankings.map((r, rank) => (
+      <Row key={r.userId}>
+        <Link
+          as={`/contest/1/rankings/${r.userId}`}
+          href={`/ranking-details?contest_id=1&user_id=${r.userId}`}
+        >
+          <RowLink href="">
+            <Rank>{rank + 1}</Rank>
+            <Name>{r.userDisplayName}</Name>
+            <Pages>
+              {amountToPages(r.amount)}
+              <span> pages</span>
+            </Pages>
+          </RowLink>
+        </Link>
+      </Row>
+    ))}
+  </List>
+)
+
+export default RankingList
 
 const List = styled.ul`
   list-style: none;
@@ -12,11 +41,22 @@ const List = styled.ul`
 
 const Row = styled.li`
   margin: 20px 0;
+`
+
+const RowLink = styled.a`
   padding: 20px 30px;
   border-radius: 2px;
   box-shadow: 4px 5px 15px 1px rgba(0, 0, 0, 0.08);
   display: flex;
   align-items: center;
+
+  &:hover,
+  &:focus,
+  &:active {
+    background-color: rgba(0, 0, 0, 0.02);
+    color: ${Constants.colors.primary};
+    box-shadow: 4px 5px 15px 1px rgba(0, 0, 0, 0.12);
+  }
 `
 
 const Rank = styled.div`
@@ -27,10 +67,6 @@ const Rank = styled.div`
 const Name = styled.div`
   flex: 1;
   font-size: 20px;
-
-  a {
-    display: block;
-  }
 `
 
 const Pages = styled.div`
@@ -40,31 +76,3 @@ const Pages = styled.div`
     font-size: 20px;
   }
 `
-
-interface Props {
-  rankings: Ranking[]
-}
-
-const RankingList = (props: Props) => (
-  <List>
-    {props.rankings.map((r, rank) => (
-      <Row key={r.userId}>
-        <Rank>{rank + 1}</Rank>
-        <Name>
-          <Link
-            as={`/contest/1/rankings/${r.userId}`}
-            href={`/ranking-details?contest_id=1&user_id=${r.userId}`}
-          >
-            <a href="">{r.userDisplayName}</a>
-          </Link>
-        </Name>
-        <Pages>
-          {amountToPages(r.amount)}
-          <span> pages</span>
-        </Pages>
-      </Row>
-    ))}
-  </List>
-)
-
-export default RankingList

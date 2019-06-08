f96515e7febb8ba7da6c3273d76b10fa65ce6cb2
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-06-08 16:00:31 +0900

    move navigation folder in ranking

--- frontends/web/app/ranking/components/navigation/SubmitPages.tsx
+++ frontends/web/app/ranking/components/navigation/SubmitPages.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
-import { RankingRegistration } from '../interfaces'
-import NewLogFormModal from '../components/modals/NewLogFormModal'
-import { NavigationBarLink } from '../../ui/components/navigation/index'
+import { RankingRegistration } from '../../interfaces'
+import NewLogFormModal from '../modals/NewLogFormModal'
+import { NavigationBarLink } from '../../../ui/components/navigation/index'
 
 interface Props {
   registration: RankingRegistration | undefined
--- frontends/web/app/ui/components/navigation/ActiveUserNavigationBar.tsx
+++ frontends/web/app/ui/components/navigation/ActiveUserNavigationBar.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { RankingRegistration } from '../../../ranking/interfaces'
-import SubmitPagesLink from '../../../ranking/navigation/SubmitPages'
+import SubmitPagesLink from '../../../ranking/components/navigation/SubmitPages'
 import SignOutLink from '../../../session/navigation/LogOut'
 import { User } from '../../../user/interfaces'
 import { UserInNavigation } from '../UserInNavigation'

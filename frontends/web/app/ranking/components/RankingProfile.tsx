1f75571ab440d11411e9ea2283adb67cd4e22048
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-06-08 15:32:11 +0900

    move ranking profile code to ranking component

---
+++ frontends/web/app/ranking/components/RankingProfile.tsx
@@ -0,0 +1,116 @@
+import React, { useState, useEffect } from 'react'
+import Layout from '../../ui/components/Layout'
+import ErrorPage from 'next/error'
+import { ContestLog, RankingRegistrationOverview } from '../interfaces'
+import RankingApi from '../api'
+import ContestApi from '../../contest/api'
+import ContestLogsByDayGraph from './ContestLogsByDayGraph'
+import ContestLogsList from './ContestLogsList'
+import {
+  rankingsToRegistrationOverview,
+  amountToPages,
+  pagesLabel,
+} from '../transform'
+import { Contest } from '../../contest/interfaces'
+import Cards, {
+  Card,
+  CardLabel,
+  CardContent,
+  LargeCard,
+} from '../../ui/components/Cards'
+
+interface Props {
+  contestId: number | undefined
+  userId: number | undefined
+}
+
+const RankingProfile = ({ contestId, userId }: Props) => {
+  const [loaded, setLoaded] = useState(false)
+  const [logs, setLogs] = useState([] as ContestLog[])
+  const [contest, setContest] = useState(undefined as Contest | undefined)
+  const [registration, setRegistration] = useState(undefined as
+    | RankingRegistrationOverview
+    | undefined)
+  const [updateCounter, setUpdateCounter] = useState(0)
+
+  useEffect(() => {
+    if (!contestId || !userId) {
+      return
+    }
+
+    const getLogs = async () => {
+      const [contest, logs, registration] = await Promise.all([
+        ContestApi.get(contestId),
+        RankingApi.getLogsFor(contestId, userId),
+        RankingApi.getRankingsRegistration(contestId, userId),
+      ])
+
+      setContest(contest)
+      setLogs(
+        logs.sort((a, b) => {
+          if (a.date > b.date) {
+            return 1
+          }
+          if (a.date < b.date) {
+            return -1
+          }
+          return 0
+        }),
+      )
+      setRegistration(rankingsToRegistrationOverview(registration))
+      setLoaded(true)
+    }
+
+    getLogs()
+  }, [contestId, userId, updateCounter])
+
+  if (!contestId || !userId) {
+    return <ErrorPage statusCode={404} />
+  }
+
+  if (!loaded) {
+    return <Layout>Loading...</Layout>
+  }
+
+  if (!registration || !contest) {
+    return <ErrorPage statusCode={500} />
+  }
+
+  const today = new Date()
+  today.setHours(0, 0, 0, 0)
+
+  const contestForGraphs = {
+    ...contest,
+    end: contest.end > today ? today : contest.end,
+  }
+
+  return (
+    <Layout>
+      <h1>{registration.userDisplayName}</h1>
+      <Cards>
+        <Card>
+          <CardContent>{contest.description}</CardContent>
+          <CardLabel>Round</CardLabel>
+        </Card>
+        {registration.registrations.map(r => (
+          <Card key={r.languageCode}>
+            <CardContent>{amountToPages(r.amount)}</CardContent>
+            <CardLabel>{pagesLabel(r.languageCode)}</CardLabel>
+          </Card>
+        ))}
+        <LargeCard>
+          <ContestLogsByDayGraph logs={logs} contest={contestForGraphs} />
+        </LargeCard>
+        <LargeCard>
+          <ContestLogsList
+            logs={logs}
+            registration={registration}
+            refreshData={() => setUpdateCounter(updateCounter + 1)}
+          />
+        </LargeCard>
+      </Cards>
+    </Layout>
+  )
+}
+
+export default RankingProfile

e6a4672f5cdc6106f9b1564f338b73af457208e0
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-06-11 12:50:17 +0900

    add announcement on the lp

--- frontends/web/pages/landing-page.tsx
+++ frontends/web/pages/landing-page.tsx
@@ -2,7 +2,15 @@ import React from 'react'
 import Layout from '../app/ui/components/Layout'
 
 const LandingPage = () => {
-  return <Layout>Welcome to Tadoku!</Layout>
+  return (
+    <Layout title="Welcome to Tadoku!">
+      <p>
+        We'll be running a test round from June 15th until June 30th. All
+        existing data will be wiped after this. Registrations are open now.
+        Please sign up to participate.
+      </p>
+    </Layout>
+  )
 }
 
 export default LandingPage

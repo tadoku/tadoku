3589aa945bdfd14724ecdc1d75580caedaff8cae
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-06-14 21:41:18 +0900

    render updates list on mobile

--- frontends/web/app/ranking/components/ContestLogsList.tsx
+++ frontends/web/app/ranking/components/ContestLogsList.tsx
@@ -90,9 +90,50 @@ const ContestLogList = (props: Props) => {
           ))}
         </Body>
       </TableList>
+      <SmallList>
+        {props.logs.map((l, i) => (
+          <SmallRow even={i % 2 === 0} key={l.id}>
+            <span>{l.date.toLocaleString()}</span>
+            <span>{languageNameByCode(l.languageCode)}</span>
+            <span>{mediumDescriptionById(l.mediumId)}</span>
+            <span>{amountToPages(l.amount)}</span>
+            <span>{amountToPages(l.adjustedAmount)}</span>
+            {canEdit && (
+              <span style={{ width: '1px', whiteSpace: 'nowrap' }}>
+                <ButtonContainer>
+                  <Button onClick={() => setSelectedLog(l)} icon="edit">
+                    Edit
+                  </Button>
+                  <Button onClick={() => deleteLog(l)} icon="trash" destructive>
+                    Delete
+                  </Button>
+                </ButtonContainer>
+              </span>
+            )}
+          </SmallRow>
+        ))}
+      </SmallList>
     </>
   )
 }
+
+const SmallList = styled.ul`
+  list-style: none;
+  padding: 0;
+  margin: 0 auto;
+  width: 100%;
+
+  ${media.greaterThan('medium')`
+    display: none;
+  `}
+`
+
+const SmallRow = styled.li`
+  margin: 20px 0;
+  padding: 20px 30px;
+  background-color: ${({ even }: { even?: boolean }) =>
+    even ? 'rgba(0, 0, 0, 0.02)' : 'transparant'};
+`
 
 const mapStateToProps = (state: State, props: Props) => ({
   ...props,
@@ -102,7 +143,6 @@ const mapStateToProps = (state: State, props: Props) => ({
 export default connect(mapStateToProps)(ContestLogList)
 
 const TableList = styled.table`
-  list-style: none;
   padding: 0;
   margin: 0 auto;
   width: 100%;

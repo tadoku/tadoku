a2b019e91fc34ce9135265ebf9457c5413bc4fa9
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-06-16 16:52:46 +0900

    extract dropdown component

--- frontends/web/app/session/components/navigation/UserMenu.tsx
+++ frontends/web/app/session/components/navigation/UserMenu.tsx
@@ -1,128 +1,18 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { User } from '../../interfaces'
-import { Button } from '../../../ui/components'
 import SignOutLink from './LogOut'
-import styled, { keyframes, css } from 'styled-components'
-import Constants from '../../../ui/Constants'
+import Dropdown, { DropdownItem } from '../../../ui/components/Dropdown'
 
 interface Props {
   user: User
 }
 
-const UserProfile = ({ user }: Props) => {
-  const [isMenuOpen, setIsMenuOpen] = useState(true)
-
-  return (
-    <Container>
-      <Button
-        onClick={() => setIsMenuOpen(!isMenuOpen)}
-        icon={isMenuOpen ? 'chevron-up' : 'chevron-down'}
-        plain
-        alignIconRight
-        style={{
-          position: 'relative',
-          zIndex: 3,
-          margin: '0 20px 0 0',
-          textDecoration: 'none',
-        }}
-      >
-        {user.displayName}
-      </Button>
-      <DropDown open={isMenuOpen}>
-        <DropDownItem>
-          <SignOutLink />
-        </DropDownItem>
-      </DropDown>
-      <DropDownOverlay open={isMenuOpen} onClick={() => setIsMenuOpen(false)} />
-    </Container>
-  )
-}
+const UserProfile = ({ user }: Props) => (
+  <Dropdown label={user.displayName}>
+    <DropdownItem>
+      <SignOutLink></SignOutLink>
+    </DropdownItem>
+  </Dropdown>
+)
 
 export default UserProfile
-
-const Container = styled.div`
-  position: relative;
-`
-
-const show = keyframes`
-  from {
-      opacity: 0;
-  }
-  to {
-      opacity: 1;
-  }
-`
-
-const DropDown = styled.ul`
-  display: none;
-  position: absolute;
-  top: 0;
-  right: 0px;
-  z-index: 2;
-  width: calc(100% + 20px);
-  min-width: 120px;
-  list-style: none;
-  box-sizing: border-box;
-  margin: 0;
-  padding: 48px 0 0;
-
-  background: ${Constants.colors.light};
-  box-shadow: 0 2px 4px 0px rgba(0, 0, 0, 0.08);
-  border-radius: 2px;
-  border: 1px solid ${Constants.colors.lightGray};
-
-  ${({ open }: { open: boolean }) =>
-    open &&
-    css`
-      display: block;
-      animation: ${show} 0.3s ease;
-    `}
-`
-
-const DropDownOverlay = styled.div`
-  display: none;
-  position: fixed;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  z-index: 1;
-  margin: 0 !important;
-
-  background: none;
-
-  ${({ open }: { open: boolean }) =>
-    open &&
-    css`
-      display: block;
-      animation: ${show} 0.3s ease;
-    `}
-`
-
-const DropDownItem = styled.li`
-  padding: 0;
-  margin: 0;
-  transition: all 0.2s ease;
-
-  button,
-  a {
-    padding: 2px 12px;
-    margin: 0;
-    width: 100%;
-    border-radius: 0;
-
-    &:last-child {
-      border-radius: 0 0 2px 2px;
-    }
-
-    &:hover:not([disabled]),
-    &:active:not([disabled]) {
-      background: ${Constants.colors.primary};
-      color: white;
-    }
-  }
-
-  & + & {
-    border-top: 1px solid ${Constants.colors.lightGray};
-  }
-`
---
+++ frontends/web/app/ui/components/Dropdown.tsx
@@ -0,0 +1,122 @@
+import React, { useState, SFC } from 'react'
+import { Button } from './index'
+import styled, { keyframes, css } from 'styled-components'
+import Constants from '../Constants'
+
+interface Props {
+  label: string
+}
+
+const Dropdown: SFC<Props> = ({ label, children }) => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <Container>
+      <Button
+        onClick={() => setIsOpen(!isOpen)}
+        icon={isOpen ? 'chevron-up' : 'chevron-down'}
+        plain
+        alignIconRight
+        style={{
+          position: 'relative',
+          zIndex: 3,
+          margin: '0 20px 0 0',
+          textDecoration: 'none',
+        }}
+      >
+        {label}
+      </Button>
+      <StyledDropdown open={isOpen}>{children}</StyledDropdown>
+      <DropdownOverlay open={isOpen} onClick={() => setIsOpen(false)} />
+    </Container>
+  )
+}
+
+export default Dropdown
+
+const Container = styled.div`
+  position: relative;
+`
+
+const show = keyframes`
+  from {
+      opacity: 0;
+  }
+  to {
+      opacity: 1;
+  }
+`
+
+const StyledDropdown = styled.ul`
+  display: none;
+  position: absolute;
+  top: 0;
+  right: 0px;
+  z-index: 2;
+  width: calc(100% + 20px);
+  min-width: 120px;
+  list-style: none;
+  box-sizing: border-box;
+  margin: 0;
+  padding: 48px 0 0;
+
+  background: ${Constants.colors.light};
+  box-shadow: 0 2px 4px 0px rgba(0, 0, 0, 0.08);
+  border-radius: 2px;
+  border: 1px solid ${Constants.colors.lightGray};
+
+  ${({ open }: { open: boolean }) =>
+    open &&
+    css`
+      display: block;
+      animation: ${show} 0.3s ease;
+    `}
+`
+
+const DropdownOverlay = styled.div`
+  display: none;
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  z-index: 1;
+  margin: 0 !important;
+
+  background: none;
+
+  ${({ open }: { open: boolean }) =>
+    open &&
+    css`
+      display: block;
+      animation: ${show} 0.3s ease;
+    `}
+`
+
+export const DropdownItem = styled.li`
+  padding: 0;
+  margin: 0;
+  transition: all 0.2s ease;
+
+  button,
+  a {
+    padding: 2px 12px;
+    margin: 0;
+    width: 100%;
+    border-radius: 0;
+
+    &:last-child {
+      border-radius: 0 0 2px 2px;
+    }
+
+    &:hover:not([disabled]),
+    &:active:not([disabled]) {
+      background: ${Constants.colors.primary};
+      color: white;
+    }
+  }
+
+  & + & {
+    border-top: 1px solid ${Constants.colors.lightGray};
+  }
+`

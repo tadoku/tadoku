1b399526fbb12bbc0e8274afeef0ac908f888531
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-06-16 20:34:31 +0900

    add change password form

---
+++ frontends/web/app/user/components/forms/ChangePasswordForm.tsx
@@ -0,0 +1,115 @@
+import React, { FormEvent, useState } from 'react'
+import {
+  Form,
+  Label,
+  LabelText,
+  Input,
+  Group,
+  ErrorMessage,
+  GroupError,
+} from '../../../ui/components/Form'
+import { Button, ButtonContainer } from '../../../ui/components'
+import { validatePassword } from '../../../session/domain'
+import UserApi from '../../api'
+
+const ChangePasswordForm = () => {
+  const [currentPassword, setcurrentPassword] = useState('')
+  const [newPassword, setNewPassword] = useState('')
+  const [newPasswordConfirmation, setNewPasswordConfirmation] = useState('')
+  const [error, setError] = useState(undefined as string | undefined)
+  const [message, setMessage] = useState(undefined as string | undefined)
+
+  const validate = () =>
+    validatePassword(currentPassword) &&
+    validatePassword(newPassword) &&
+    newPassword === newPasswordConfirmation
+
+  const submit = async (event: FormEvent) => {
+    event.preventDefault()
+
+    const response = await UserApi.changePassword({
+      currentPassword,
+      newPassword,
+    })
+
+    if (!response) {
+      setError('Old password is incorrect.')
+      return
+    }
+
+    setcurrentPassword('')
+    setNewPassword('')
+    setNewPasswordConfirmation('')
+    setError(undefined)
+    setMessage('Your password has been changed.')
+  }
+
+  const hasError = {
+    form: !validate(),
+    currentPassword:
+      currentPassword != '' && !validatePassword(currentPassword),
+    newPassword: newPassword != '' && !validatePassword(newPassword),
+    newPasswordConfirmation:
+      newPasswordConfirmation != '' && newPassword !== newPasswordConfirmation,
+  }
+
+  return (
+    <Form onSubmit={submit}>
+      <ErrorMessage message={error} />
+      <ErrorMessage message={message} />
+      <Group>
+        <Label>
+          <LabelText>Old password</LabelText>
+          <Input
+            type="password"
+            value={currentPassword}
+            onChange={e => setcurrentPassword(e.target.value)}
+            error={hasError.currentPassword}
+          />
+          <GroupError
+            message="Password should be at least 6 characters"
+            hidden={!hasError.currentPassword}
+          />
+        </Label>
+      </Group>
+      <Group>
+        <Label>
+          <LabelText>New password</LabelText>
+          <Input
+            type="password"
+            value={newPassword}
+            onChange={e => setNewPassword(e.target.value)}
+            error={hasError.newPassword}
+          />
+        </Label>
+        <GroupError
+          message="Password should be at least 6 characters"
+          hidden={!hasError.newPassword}
+        />
+      </Group>
+      <Group>
+        <Label>
+          <LabelText>New password confirmation</LabelText>
+          <Input
+            type="password"
+            value={newPasswordConfirmation}
+            onChange={e => setNewPasswordConfirmation(e.target.value)}
+            error={hasError.newPasswordConfirmation}
+          />
+        </Label>
+        <GroupError
+          message="Password confirmation should be identical"
+          hidden={!hasError.newPasswordConfirmation}
+        />
+      </Group>
+      <Group>
+        <ButtonContainer noMargin>
+          <Button type="submit" disabled={hasError.form}>
+            Update password
+          </Button>
+        </ButtonContainer>
+      </Group>
+    </Form>
+  )
+}
+export default ChangePasswordForm

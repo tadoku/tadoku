b4cfeeadcff79ec51c872be04b99bdab58813619
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-06-16 18:34:08 +0900

    prevent logged out users from accessing the settings page

--- frontends/web/app/user/pages/Settings.tsx
+++ frontends/web/app/user/pages/Settings.tsx
@@ -1,13 +1,32 @@
 import React from 'react'
+import ErrorPage from 'next/error'
 import Layout from '../../ui/components/Layout'
 import { SettingsTab } from '../interfaces'
+import { connect } from 'react-redux'
+import { State } from '../../store'
+import { User } from '../../session/interfaces'
 
 interface Props {
   tab: SettingsTab
+  user: User | undefined
+  userLoaded: boolean
 }
 
-const Settings = ({ tab }: Props) => {
+const Settings = ({ tab, user, userLoaded }: Props) => {
+  if (!userLoaded) {
+    return <Layout title="Settings">Loading...</Layout>
+  }
+
+  if (userLoaded && !user) {
+    return <ErrorPage statusCode={404} />
+  }
+
   return <Layout title="Settings">Active tab: {tab}</Layout>
 }
 
-export default Settings
+const mapStateToProps = (state: State) => ({
+  user: state.session.user,
+  userLoaded: state.session.loaded,
+})
+
+export default connect(mapStateToProps)(Settings)

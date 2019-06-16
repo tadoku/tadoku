6117d40bc21c0a3ec24d991b2f8b2e49595b3a2b
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-06-16 12:45:55 +0900

    add serializer for ranking registration

--- frontends/web/app/ranking/transform.ts
+++ frontends/web/app/ranking/transform.ts
@@ -5,6 +5,8 @@ import {
   RankingRegistrationOverview,
   rawRanking,
   rawContestLog,
+  RankingRegistration,
+  rawRankingRegistration,
 } from './interfaces'
 import { Contest } from './../contest/interfaces'
 import { languageNameByCode } from './database'
@@ -74,6 +76,37 @@ export const ContestLogsSerializer: Serializer<ContestLog[]> = {
   deserialize: serializedData => {
     let raw = JSON.parse(serializedData)
     return raw.map(RawToContestLogMapper)
+  },
+}
+
+export const RawToRankingRegistrationMapper: Mapper<
+  rawRankingRegistration,
+  RankingRegistration
+> = raw => ({
+  contestId: raw.contest_id,
+  languages: raw.languages,
+  start: new Date(raw.start),
+  end: new Date(raw.end),
+})
+
+export const RankingRegistrationToRawMapper: Mapper<
+  RankingRegistration,
+  rawRankingRegistration
+> = registration => ({
+  contest_id: registration.contestId,
+  languages: registration.languages,
+  start: registration.start.toISOString(),
+  end: registration.end.toISOString(),
+})
+
+export const RankingRegistrationSerializer: Serializer<RankingRegistration> = {
+  serialize: data => {
+    const raw = RankingRegistrationToRawMapper(data)
+    return JSON.stringify(raw)
+  },
+  deserialize: serializedData => {
+    let raw = JSON.parse(serializedData)
+    return RawToRankingRegistrationMapper(raw)
   },
 }
 

8032759245669f498e0d65b5ef4cadf843f89529
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-06-16 15:01:26 +0900

    toggle menu on click

--- frontends/web/app/session/components/navigation/UserProfile.tsx
+++ frontends/web/app/session/components/navigation/UserProfile.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { User } from '../../interfaces'
 import { Button } from '../../../ui/components'
 
@@ -6,10 +6,22 @@ interface Props {
   user: User
 }
 
-const UserProfile = ({ user }: Props) => (
-  <Button icon="chevron-down" plain alignIconRight>
-    {user.displayName}
-  </Button>
-)
+const UserProfile = ({ user }: Props) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  return (
+    <>
+      <Button
+        onClick={() => setIsMenuOpen(!isMenuOpen)}
+        icon="chevron-down"
+        plain
+        alignIconRight
+      >
+        {user.displayName}
+      </Button>
+      {isMenuOpen && 'open'}
+    </>
+  )
+}
 
 export default UserProfile

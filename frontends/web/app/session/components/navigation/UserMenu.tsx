0c2ec83874b45e0fd36980aebad153dabb61600d
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-06-16 15:19:11 +0900

    add styling for dropdown

--- frontends/web/app/session/components/navigation/UserMenu.tsx
+++ frontends/web/app/session/components/navigation/UserMenu.tsx
@@ -2,16 +2,18 @@ import React, { useState } from 'react'
 import { User } from '../../interfaces'
 import { Button } from '../../../ui/components'
 import SignOutLink from './LogOut'
+import styled from 'styled-components'
+import Constants from '../../../ui/Constants'
 
 interface Props {
   user: User
 }
 
 const UserProfile = ({ user }: Props) => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const [isMenuOpen, setIsMenuOpen] = useState(true)
 
   return (
-    <>
+    <Container>
       <Button
         onClick={() => setIsMenuOpen(!isMenuOpen)}
         icon="chevron-down"
@@ -21,14 +23,47 @@ const UserProfile = ({ user }: Props) => {
         {user.displayName}
       </Button>
       {isMenuOpen && (
-        <ul>
-          <li>
+        <DropDown>
+          <DropDownItem>
             <SignOutLink />
-          </li>
-        </ul>
+          </DropDownItem>
+        </DropDown>
       )}
-    </>
+    </Container>
   )
 }
 
 export default UserProfile
+
+const Container = styled.div`
+  position: relative;
+`
+
+const DropDown = styled.ul`
+  position: absolute;
+  top: 40px;
+  width: 100%;
+
+  list-style: none;
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+
+  background: ${Constants.colors.light};
+  box-shadow: 0 2px 4px 0px rgba(0, 0, 0, 0.08);
+  border-radius: 2px;
+  border: 1px solid ${Constants.colors.lightGray};
+`
+
+const DropDownItem = styled.li`
+  padding: 2px 12px;
+  margin: 0;
+
+  &:hover {
+    background: ${Constants.colors.lightGray};
+  }
+
+  & + & {
+    border-top: 1px solid ${Constants.colors.lightGray};
+  }
+`

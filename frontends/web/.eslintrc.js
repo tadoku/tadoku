3452c7594694dbfd77364bc3bf1607065d03bc48
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-06-17 23:17:20 +0900

    disable explicit member accessibility rule

--- frontends/web/.eslintrc.js
+++ frontends/web/.eslintrc.js
@@ -20,6 +20,7 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/camelcase': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-member-accessibility': 'off',
     '@typescript-eslint/member-delimiter-style': {
       multiline: {
         delimiter: 'none',

8dd13b01a33403f58697c5130ffb568cb4fc8d26
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-06-17 22:10:25 +0900

    add eslint to scripts

--- frontends/web/.eslintrc.js
+++ frontends/web/.eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   parser: '@typescript-eslint/parser', // Specifies the ESLint parser
   extends: [
-    'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
+    'plugin:react/recommended', // Uses the recommended rules from eslint-plugin-react
     'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
     'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],

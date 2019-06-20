f8e3a6ddb27df11ac6c04e42e2e085d10d6edb95
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-06-21 00:22:16 +0900

    make activity indicator look nicer

--- frontends/web/app/ui/components/ActivityIndicator.tsx
+++ frontends/web/app/ui/components/ActivityIndicator.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Button } from './index'
 
 interface Props {
   isLoading: boolean
 }
 
 const ActivityIndicator = ({ isLoading }: Props) =>
-  isLoading ? <span>Loading...</span> : null
+  isLoading ? <Button plain loading disabled /> : null
 
 export default ActivityIndicator

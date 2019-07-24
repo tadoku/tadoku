7960c0f643fbe848c53164c57513d44cd0cef7c9
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-07-24 17:11:31 +0900

    migrate server to next 9

--- frontends/web/server/index.ts
+++ frontends/web/server/index.ts
@@ -1,45 +1,39 @@
-import * as next from 'next'
-import * as Express from 'express'
+import next from 'next'
+import express from 'express'
 
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
-app
-  .prepare()
-  .then(() => {
-    const server = Express()
+app.prepare().then(() => {
+  const server = express()
 
-    server.get('/contest/:contest_id/rankings/:user_id', (req, res) => {
-      const page = '/ranking-details'
-      const queryParams = {
-        contest_id: req.params.contest_id,
-        user_id: req.params.user_id,
-      }
-      app.render(req, res, page, queryParams)
-    })
-
-    server.get('/settings/:tab', (req, res) => {
-      const page = '/settings'
-      const queryParams = {
-        tab: req.params.tab,
-      }
-      app.render(req, res, page, queryParams)
-    })
-
-    server.get('*', (req, res) => {
-      return handle(req, res)
-    })
-
-    server.listen(3000, (err: Error) => {
-      if (err) {
-        throw err
-      }
-
-      console.log('> Ready and accepting requests')
-    })
+  server.get('/contest/:contest_id/rankings/:user_id', (req, res) => {
+    const page = '/ranking-details'
+    const queryParams = {
+      contest_id: req.params.contest_id,
+      user_id: req.params.user_id,
+    }
+    app.render(req, res, page, queryParams)
   })
-  .catch(ex => {
-    console.error(ex.stack)
-    process.exit(1)
+
+  server.get('/settings/:tab', (req, res) => {
+    const page = '/settings'
+    const queryParams = {
+      tab: req.params.tab,
+    }
+    app.render(req, res, page, queryParams)
   })
+
+  server.get('*', (req, res) => {
+    return handle(req, res)
+  })
+
+  server.listen(3000, (err: Error) => {
+    if (err) {
+      throw err
+    }
+
+    console.log('> Ready and accepting requests')
+  })
+})

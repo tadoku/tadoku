278e493e804a7503584959dd91bb67d87ccb983b
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-10-14 18:02:40 +0900

    remove obsolete express type

--- frontends/web/app/interfaces.ts
+++ frontends/web/app/interfaces.ts
@@ -1,9 +1,3 @@
 import { NextPageContext } from 'next'
-import { Request, Response } from 'express'
-
-export interface ExpressNextContext extends NextPageContext {
-  req?: Request
-  res?: Response
-}
 
 export type Mapper<A, B> = (rawData: A) => B

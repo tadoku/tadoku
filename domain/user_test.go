d945b48c7c0dc5c3748a92847dc83bcd869fa889
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-12-30 13:11:39 +0900

    add validator in domain tests

--- domain/user_test.go
+++ domain/user_test.go
@@ -1,40 +1,41 @@
-package domain
+package domain_test
 
 import (
 	"encoding/json"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/tadoku/api/domain"
 )
 
 func TestUser_PasswordIsNotExported(t *testing.T) {
-	u := User{
+	u := domain.User{
 		ID:          1,
 		Email:       "foo@example.com",
 		DisplayName: "John Smith",
 		Password:    "foobar",
-		Role:        RoleUser,
-		Preferences: &Preferences{},
+		Role:        domain.RoleUser,
+		Preferences: &domain.Preferences{},
 	}
 	userJSON, err := json.Marshal(u)
 	assert.NoError(t, err)
 
-	newUser := User{}
+	newUser := domain.User{}
 	json.Unmarshal(userJSON, &newUser)
 	assert.Empty(t, newUser.Password)
 }
 
 func TestUser_Validation(t *testing.T) {
 	var tests = []struct {
-		user          User
+		user          domain.User
 		expectedError error
 	}{
-		{User{Password: "ewgflikhghewioghew"}, nil},
-		{User{}, ErrUserMissingPassword},
+		{domain.User{Password: "ewgflikhghewioghew"}, nil},
+		{domain.User{}, domain.ErrUserMissingPassword},
 	}
 
 	for _, test := range tests {
-		_, err := test.user.Validate()
+		_, err := validate(test.user)
 
 		if test.expectedError != nil {
 			assert.EqualErrorf(t, err, test.expectedError.Error(), "expected user.Validate of %v to be %v, got %v instead", test.user, test.expectedError, err)

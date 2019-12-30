59c120f3be3b35bc242225170c54dadffb329e91
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-12-30 15:53:54 +0900

    fix storybook config

--- frontends/web/.storybook/config.tsx
+++ frontends/web/.storybook/config.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { configure } from '@storybook/react'
 import { addDecorator } from '@storybook/react'
-import { GlobalStyle } from '../app/ui/components/Layout'
-import Constants from '../app/ui/Constants'
+import { GlobalStyle } from '../src/app/ui/components/Layout'
+import Constants from '../src/app/ui/Constants'
 
-const req = require.context('../app', true, /.stories.tsx$/)
+const req = require.context('../src/app', true, /.stories.tsx$/)
 function loadStories() {
   req.keys().forEach(filename => req(filename))
 }

e51a7f85b8749fa09c2733ca4f2e1216ec627bc6
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-12-30 15:54:10 +0900

    fix footer component story with a correct container

--- frontends/web/src/app/ui/components/Footer.stories.tsx
+++ frontends/web/src/app/ui/components/Footer.stories.tsx
@@ -2,4 +2,8 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import Footer from './Footer'
 
-storiesOf('Footer', module).add('default', () => <Footer />)
+storiesOf('Footer', module).add('default', () => (
+  <div style={{ height: '300px', width: '100%', position: 'relative' }}>
+    <Footer />
+  </div>
+))

394f1d0e3c6faba5961e6b300ff498c8af4dab58
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-12-30 18:11:13 +0900

    add blog post component

--- frontends/web/src/app/blog/pages/BlogsList.tsx
+++ frontends/web/src/app/blog/pages/BlogsList.tsx
@@ -3,10 +3,11 @@ import BlogApi from '../api'
 import { PageTitle } from '../../ui/components'
 import { useCachedApiState } from '../../cache'
 import { Post } from '../domain'
+import BlogPost from '../components/BlogPost'
 
 interface Props {}
 
-const BlogsList = ({  }: Props) => {
+const BlogsList = ({}: Props) => {
   const { data: posts } = useCachedApiState<Post[]>({
     cacheKey: `blog_list?i=1`,
     defaultValue: [],
@@ -17,11 +18,9 @@ const BlogsList = ({  }: Props) => {
   return (
     <>
       <PageTitle>Blog</PageTitle>
-      <ul>
-        {posts.map(p => (
-          <li key={p.uuid}>{p.html}</li>
-        ))}
-      </ul>
+      {posts.map(p => (
+        <BlogPost key={p.uuid} post={p} />
+      ))}
     </>
   )
 }

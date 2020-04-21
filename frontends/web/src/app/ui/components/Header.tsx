34f055ec09f9ebb3b552301668f1efe3cd12acf0
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2020-04-21 22:51:55 +0900

    close menu by default

--- frontends/web/src/app/ui/components/Header.tsx
+++ frontends/web/src/app/ui/components/Header.tsx
@@ -14,7 +14,7 @@ interface Props {
 }
 
 const Header = ({ isLoading }: Props) => {
-  const [isOpen, setIsOpen] = useState(true)
+  const [isOpen, setIsOpen] = useState(false)
 
   return (
     <Container>

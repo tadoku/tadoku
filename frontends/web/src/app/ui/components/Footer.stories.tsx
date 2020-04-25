81f546cf725b0569b2e2d71fd29e314c08a65e7c
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2020-04-25 22:58:27 +0900

    fix footer storybook

--- frontends/web/src/app/ui/components/Footer.stories.tsx
+++ frontends/web/src/app/ui/components/Footer.stories.tsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import Footer from './Footer'
+import { Contest } from '../../contest/interfaces'
+
+const contests: Contest[] = [
+  {
+    id: 1,
+    description: '2020 Round 1',
+    start: new Date(),
+    end: new Date(),
+    open: false,
+  },
+  {
+    id: 2,
+    description: '2020 Round 1',
+    start: new Date(),
+    end: new Date(),
+    open: false,
+  },
+]
 
 storiesOf('Footer', module).add('default', () => (
   <div style={{ height: '300px', width: '100%', position: 'relative' }}>
-    <Footer />
+    <Footer contests={contests} />
   </div>
 ))

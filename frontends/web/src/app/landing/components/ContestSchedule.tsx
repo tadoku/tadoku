5d3718b4e034c5f3aca90e5560f147ee8823eb3c
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2020-04-27 15:01:17 +0900

    add background to contest schedule

--- frontends/web/src/app/landing/components/ContestSchedule.tsx
+++ frontends/web/src/app/landing/components/ContestSchedule.tsx
@@ -7,19 +7,28 @@ import media from 'styled-media-query'
 const ContestSchedule = () => (
   <Container>
     <Title>Contest schedule</Title>
-    <Schedule>
-      <Contest
-        round="1"
-        description="January 1st - February 14th (1.5 months)"
-      />
-      <Contest round="2" description="March 24th - April 7th (2 weeks)" />
-      <Contest round="3" description="May 1st - 31st (1 month)" />
-      <Contest round="4" description="June 23rd - July 7th (2 weeks)" />
-      <Contest round="5" description="July 24th - August 7th (2 weeks)" />
-      <Contest round="6" description="September 1st - 30th (1 month)" />
-      <Contest round="7" description="October 24th - November 7th (2 weeks)" />
-      <Contest round="8" description="November 23rd - December 7th (2 weeks)" />
-    </Schedule>
+    <BackgroundContainer>
+      <Schedule>
+        <Contest
+          round="1"
+          description="January 1st - February 14th (1.5 months)"
+        />
+        <Contest round="2" description="March 24th - April 7th (2 weeks)" />
+        <Contest round="3" description="May 1st - 31st (1 month)" />
+        <Contest round="4" description="June 23rd - July 7th (2 weeks)" />
+        <Contest round="5" description="July 24th - August 7th (2 weeks)" />
+        <Contest round="6" description="September 1st - 30th (1 month)" />
+        <Contest
+          round="7"
+          description="October 24th - November 7th (2 weeks)"
+        />
+        <Contest
+          round="8"
+          description="November 23rd - December 7th (2 weeks)"
+        />
+      </Schedule>
+      <Background />
+    </BackgroundContainer>
   </Container>
 )
 
@@ -29,6 +38,22 @@ const Container = styled.div`
   width: 100%;
   padding: 60px 60px 0 60px;
   box-sizing: border-box;
+`
+
+const BackgroundContainer = styled.div`
+  position: relative;
+`
+
+const Background = styled.div`
+  position: absolute;
+  background: #bbb;
+  right: 0px;
+  bottom: 60px;
+  top: 60px;
+  width: 100%;
+  max-width: 680px;
+  z-index: -1;
+  background: bottom right no-repeat url('/img/background-contests.jpg');
 `
 
 const Title = styled.h2`

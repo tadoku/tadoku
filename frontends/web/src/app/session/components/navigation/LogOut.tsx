63069f8b75c46b93ee17d811b8f0f0cf315d589d
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2020-04-30 09:30:54 +0900

    fix log out infinite loop

--- frontends/web/src/app/session/components/navigation/LogOut.tsx
+++ frontends/web/src/app/session/components/navigation/LogOut.tsx
@@ -3,16 +3,17 @@ import Router from 'next/router'
 import { useDispatch } from 'react-redux'
 
 import { Button } from '../../../ui/components'
+import { logOut } from '../../redux'
 
 export const LogOut = () => {
   const dispatch = useDispatch()
-  const logOut = () => {
+  const logOutHandler = () => {
     dispatch(logOut())
     Router.push('/')
   }
 
   return (
-    <Button plain icon="sign-out-alt" onClick={logOut}>
+    <Button plain icon="sign-out-alt" onClick={logOutHandler}>
       Log out
     </Button>
   )

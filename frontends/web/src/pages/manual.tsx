5b765a783eeaf6e7c0b9f08db54a811e8ced4e3b
gnerkus <ifeanyioraelosi@gmail.com>
2020-07-03 16:02:59 +0200

    use absolute import paths for app/blog

--- frontends/web/src/pages/manual.tsx
+++ frontends/web/src/pages/manual.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Head from 'next/head'
-import PostOrPageDetail from '../app/blog/pages/PostOrPageDetail'
+import PostOrPageDetail from '@app/blog/pages/PostOrPageDetail'
 
 const Manual = () => (
   <>

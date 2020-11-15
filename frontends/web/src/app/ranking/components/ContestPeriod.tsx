ad72ba9c6d8ad5d7dbf397ba4054b4488639ba06
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2020-11-15 18:54:27 +0900

    add timer for contests

--- frontends/web/src/app/ranking/components/ContestPeriod.tsx
+++ frontends/web/src/app/ranking/components/ContestPeriod.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { format } from 'date-fns'
 import styled from 'styled-components'
 import media from 'styled-media-query'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { formatDistanceToNow } from 'date-fns'
 
 import { Contest } from '@app/contest/interfaces'
 import Constants from '@app/ui/Constants'
@@ -18,31 +19,80 @@ const ContestPeriod = ({ contest }: Props) => {
 
   return (
     <Container>
-      <Box>
-        <Label>{startingLabel}</Label>
-        <Value>{format(contest.start, 'MMMM dd')}</Value>
-      </Box>
-      <Icon icon="arrow-right" />
-      <Box>
-        <Label>{endingLabel}</Label>
-        <Value>{format(contest.end, 'MMMM dd')}</Value>
-      </Box>
+      <Dates>
+        <Box>
+          <Label>{startingLabel}</Label>
+          <Value>{format(contest.start, 'MMMM dd')}</Value>
+        </Box>
+        <Icon icon="arrow-right" />
+        <Box>
+          <Label>{endingLabel}</Label>
+          <Value>{format(contest.end, 'MMMM dd')}</Value>
+        </Box>
+      </Dates>
+      <RemainingTime contest={contest} />
     </Container>
   )
 }
 
 export default ContestPeriod
 
+const RemainingTime = ({ contest }: Props) => {
+  const [now, setNow] = useState(() => new Date())
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      setNow(new Date())
+    }, 1000)
+    return () => clearInterval(id)
+  }, [])
+
+  const startingLabel = formatDistanceToNow(contest.start, {
+    includeSeconds: true,
+  })
+  const endingLabel = formatDistanceToNow(contest.end, {
+    includeSeconds: true,
+  })
+
+  if (now < contest.start) {
+    return (
+      <RemainingBlock>
+        Starts in <strong>{startingLabel}</strong>
+      </RemainingBlock>
+    )
+  }
+
+  if (now < contest.end) {
+    return (
+      <RemainingBlock>
+        Ends in <strong>{endingLabel}</strong>
+      </RemainingBlock>
+    )
+  }
+
+  return <RemainingBlock>Contest has ended</RemainingBlock>
+}
+
+const RemainingBlock = styled.div`
+  font-size: 12px;
+  margin: 10px -15px -10px -15px;
+  padding: 5px 15px;
+  border-top: 1px solid ${Constants.colors.lightGray};
+`
+
 const Container = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
   padding: 10px 15px;
   box-shadow: 0px 2px 3px 0px rgba(0, 0, 0, 0.08);
   box-sizing: border-box;
   background: ${Constants.colors.light};
 
   ${media.lessThan('small')`width: 100%; margin-top: 5px;`}
+`
+
+const Dates = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `
 
 const Icon = styled(FontAwesomeIcon)`

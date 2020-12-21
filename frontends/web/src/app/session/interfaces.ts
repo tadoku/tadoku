38d4d0321353ce5c188776aba973b4e7d6b74dd3
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2020-12-21 14:00:34 +0900

    add role to user

--- frontends/web/src/app/session/interfaces.ts
+++ frontends/web/src/app/session/interfaces.ts
@@ -2,10 +2,12 @@ export interface User {
   id: number
   email: string
   displayName: string
+  role: number
 }
 
 export interface RawUser {
   id: number
   email: string
   display_name: string
+  role: number
 }

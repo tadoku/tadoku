15b378bae6a03e48200a47a51af7048b32dbe24e
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2021-05-22 21:16:45 +0900

    add health endpoint

--- services/blog/main.go
+++ services/blog/main.go
@@ -15,6 +15,10 @@ func main() {
 		fmt.Fprint(w, posts)
 	})
 
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "ok")
+	})
+
 	log.Print("Starting server on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }

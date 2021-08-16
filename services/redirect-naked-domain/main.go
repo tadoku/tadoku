7e25e08985df6bde208c38ac07839c09dee905a5
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2021-08-16 14:30:25 +0900

    remove test code

--- services/redirect-naked-domain/main.go
+++ services/redirect-naked-domain/main.go
@@ -32,20 +32,7 @@ func main() {
 		}
 
 		target := RemoveWWWSubdomain(host)
-		// http.Redirect(w, r, target.String(), 301)
-
-		fmt.Fprintf(w, "will redirect to: %s", target.String())
-
-		fmt.Fprintf(w, "%s %s %s \n", r.Method, r.URL, r.Proto)
-		//Iterate over all header fields
-		for k, v := range r.Header {
-			fmt.Fprintf(w, "Header field %q, Value %q\n", k, v)
-		}
-
-		fmt.Fprintf(w, "Host = %q\n", r.Host)
-		fmt.Fprintf(w, "RemoteAddr= %q\n", r.RemoteAddr)
-		//Get value for a specified token
-		fmt.Fprintf(w, "\n\nFinding value of \"Accept\" %q", r.Header["Accept"])
+		http.Redirect(w, r, target.String(), 301)
 	})
 
 	log.Print("Starting server on port 8080")

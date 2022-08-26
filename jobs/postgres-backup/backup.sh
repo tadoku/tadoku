70e7a0b83ce18c00c9a8af22f40be5ae05258dfc
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2022-08-27 00:01:07 +0900

    move bzip step

--- jobs/postgres-backup/backup.sh
+++ jobs/postgres-backup/backup.sh
@@ -2,7 +2,6 @@
 
 export DUMP_FILE=/backup_${PRODUCT_NAME}_`date +%Y%m%d_%H%M%S`.pgdump
 PGPASSWORD=$POSTGRES_PASSWORD pg_dump -d $POSTGRES_DB -U $POSTGRES_USER -h $POSTGRES_HOST -f $DUMP_FILE
-bzip2 $DUMP_FILE
 
 # debugging
 if [ "$DEBUG" == 'true' ]
@@ -20,4 +19,5 @@ then
   echo "aws s3 cp ${DUMP_FILE}.bz2 ${S3_BACKUP_PATH}"
 fi
 
+bzip2 $DUMP_FILE
 aws s3 cp ${DUMP_FILE}.bz2 $S3_BACKUP_PATH

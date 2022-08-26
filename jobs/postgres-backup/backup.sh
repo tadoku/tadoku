7967a7154db1317b7b3a24ee0c6b82e85028f464
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2022-08-26 22:54:00 +0900

    create manual postgres-backup

---
+++ jobs/postgres-backup/backup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export DUMP_FILE=/backup_`$PRODUCT_NAME`_`date +%Y%m%d_%H%M%S`.pgdump
+PGPASSWORD=$POSTGRES_PASSWORD pg_dump -d $POSTGRES_DB -U $POSTGRES_USER -h $POSTGRES_HOST -f $DUMP_FILE
+bzip2 $DUMP_FILE
+aws s3 cp ${DUMP_FILE}.bz2 $S3_BACKUP_PATH

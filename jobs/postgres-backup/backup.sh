7d530e359cb3a2e8f164b111b3ed7eb2d8e369d5
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2022-08-27 14:13:39 +0900

    add discord notifications when backups run

--- jobs/postgres-backup/backup.sh
+++ jobs/postgres-backup/backup.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-export DUMP_FILE=/backup_${PRODUCT_NAME}_`date +%Y%m%d_%H%M%S`.pgdump
-PGPASSWORD=$POSTGRES_PASSWORD pg_dump -d $POSTGRES_DB -U $POSTGRES_USER -h $POSTGRES_HOST -f $DUMP_FILE
+send_discord_notification() {
+  curl --request POST \
+    --url "$DISCORD_WEBHOOK_URL" \
+    --header 'Content-Type: application/json' \
+    --data '{ "content": "'"$0"'" }'
+}
+
+send_failure_notification() {
+  send_discord_notification ":red_circle: Tadoku database backup failed, needs investigation"
+}
+
+trap 'send_failure_notification' ERR
+
+export DUMP_FILE=/backup_${PRODUCT_NAME}_$(date +%Y%m%d_%H%M%S).pgdump
+PGPASSWORD=$POSTGRES_PASSWORD pg_dump -d "$POSTGRES_DB" -U "$POSTGRES_USER" -h "$POSTGRES_HOST" -f "$DUMP_FILE"
 
 # debugging
 if [ "$DEBUG" == 'true' ]
@@ -19,5 +32,7 @@ then
   echo "aws s3 cp ${DUMP_FILE}.bz2 ${S3_BACKUP_PATH} --storage-class GLACIER"
 fi
 
-bzip2 $DUMP_FILE
-aws s3 cp ${DUMP_FILE}.bz2 $S3_BACKUP_PATH --storage-class GLACIER
+bzip2 "$DUMP_FILE"
+aws s3 cp "${DUMP_FILE}".bz2 "$S3_BACKUP_PATH" --storage-class GLACIER
+
+send_discord_notification ":green_circle: Tadoku database ran successfully"

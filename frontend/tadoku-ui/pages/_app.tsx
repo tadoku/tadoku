5761ae1b26846ebc40a9cff1d36b04aae68ca952
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2022-12-04 15:06:04 +0900

    add menu

--- frontend/tadoku-ui/pages/_app.tsx
+++ frontend/tadoku-ui/pages/_app.tsx
@@ -1,6 +1,54 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Link from 'next/link'
+
+interface NavigationBlock {
+  title: string
+  links: {
+    title: string
+    href: string
+  }[]
+}
+
+const navigation: NavigationBlock[] = [
+  {
+    title: 'Foundation',
+    links: [
+      { title: 'Color', href: '/forms' },
+      { title: 'Typography', href: '/typography' },
+      { title: 'Templates', href: '/templates' },
+    ],
+  },
+  {
+    title: 'Components',
+    links: [
+      { title: 'Forms', href: '/forms' },
+      { title: 'Navigation', href: '/navigation' },
+    ],
+  },
+]
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="flex min-h-screen bg-neutral-100">
+      <div className="bg-white w-48 p-8">
+        <h1 className="text-2xl font-bold mb-4">tadoku-ui</h1>
+        {navigation.map(block => (
+          <>
+            <h2 className="text-l font-semibold">{block.title}</h2>
+            <ul className="mt-2 mb-4">
+              {block.links.map(l => (
+                <li className="border-l-2 border-neutral-200 pl-4 py-1 hover:border-blue-300 text-neutral-600 hover:text-neutral-900">
+                  <Link href={l.href}>{l.title}</Link>
+                </li>
+              ))}
+            </ul>
+          </>
+        ))}
+      </div>
+      <div className="p-8">
+        <Component {...pageProps} />
+      </div>
+    </div>
+  )
 }

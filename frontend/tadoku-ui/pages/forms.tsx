875fc5f223aa4cd44aac4a2d12db7768d292c93a
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2022-12-04 19:50:10 +0900

    add select

--- frontend/tadoku-ui/pages/forms.tsx
+++ frontend/tadoku-ui/pages/forms.tsx
@@ -13,6 +13,7 @@ export default function Forms() {
         </label>
       </Preview>
       <CodeBlock
+        language="html"
         code={`<label className="label">
   <span className="label-text">First name</span>
   <input type="text" placeholder="John Doe" />
@@ -29,6 +30,7 @@ export default function Forms() {
         </label>
       </Preview>
       <CodeBlock
+        language="html"
         code={`<label className="label">
   <span className="label-text">Message</span>
   <textarea placeholder="Dolor sit amet..." />
@@ -38,8 +40,31 @@ export default function Forms() {
       <Separator />
 
       <Title>Select</Title>
-      <Preview></Preview>
-      <CodeBlock code={``} />
+      <Preview>
+        <label className="label">
+          <span className="label-text">Choose a color</span>
+          <select>
+            <option value="#ff0000" selected>
+              Red
+            </option>
+            <option value="#00ff00">Green</option>
+            <option value="#0000ff">Blue</option>
+          </select>
+        </label>
+      </Preview>
+      <CodeBlock
+        language="html"
+        code={`<label className="label">
+  <span className="label-text">Choose a color</span>
+  <select>
+    <option value="#ff0000" selected>
+      Red
+    </option>
+    <option value="#00ff00">Green</option>
+    <option value="#0000ff">Blue</option>
+  </select>
+</label>`}
+      />
 
       <Separator />
 

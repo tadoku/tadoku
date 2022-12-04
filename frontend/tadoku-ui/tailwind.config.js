b11b1eee30776014d3ba2b96e3c46d1bf658edd4
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2022-12-04 18:55:43 +0900

    add tadoku fonts

--- frontend/tadoku-ui/tailwind.config.js
+++ frontend/tadoku-ui/tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}", 
@@ -9,7 +11,11 @@ module.exports = {
       colors: {
         primary: '#6969FF',
         secondary: '#1B264F',
-      }
+      },
+      fontFamily: {
+        sans: ['Open Sans', ...defaultTheme.fontFamily.sans],
+        serif: ['Merriweather', ...defaultTheme.fontFamily.serif]
+      },
     },
   },
   safelist: [

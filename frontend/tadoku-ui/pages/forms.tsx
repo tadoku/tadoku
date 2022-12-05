c103d7f38dbdc32a9032f75c8cac3f94d0790091
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2022-12-05 20:30:54 +0900

    restructure forms page in preparation of react implementation

--- frontend/tadoku-ui/pages/forms.tsx
+++ frontend/tadoku-ui/pages/forms.tsx
@@ -5,56 +5,67 @@ export default function Forms() {
     <>
       <h1 className="title mb-8">Forms</h1>
 
-      <Title>Input</Title>
+      <Title>React usage</Title>
+      <Preview></Preview>
+      <CodeBlock language="html" code={``} />
+
+      <Separator />
+
+      <Title>Basic elements</Title>
       <Preview>
-        <label className="label">
-          <span className="label-text">First name</span>
-          <input type="text" placeholder="John Doe" />
-        </label>
+        <form className="v-stack">
+          <label className="label">
+            <span className="label-text">First name</span>
+            <input type="text" placeholder="John Doe" />
+          </label>
+          <label className="label">
+            <span className="label-text">Message</span>
+            <textarea placeholder="Dolor sit amet..." />
+          </label>
+          <label className="label">
+            <span className="label-text">Choose a color</span>
+            <select>
+              <option value="#ff0000" selected>
+                Red
+              </option>
+              <option value="#00ff00">Green</option>
+              <option value="#0000ff">Blue</option>
+            </select>
+          </label>
+          <div>
+            <span className="label-text">Choose a color</span>
+            <label className="label-radio">
+              <input type="radio" name="color-radio" />
+              <span>Red</span>
+            </label>
+            <label className="label-radio">
+              <input type="radio" name="color-radio" />
+              <span>Green</span>
+            </label>
+            <label className="label-radio">
+              <input type="radio" name="color-radio" />
+              <span>Blue</span>
+            </label>
+          </div>
+          <label className="label error">
+            <span className="label-text">First name</span>
+            <input type="text" placeholder="John Doe" />
+            <span className="error">Should be at least 1 character long</span>
+          </label>
+        </form>
       </Preview>
       <CodeBlock
         language="html"
-        code={`<label className="label">
+        code={`<form className="v-stack">
+<label className="label">
   <span className="label-text">First name</span>
   <input type="text" placeholder="John Doe" />
-</label>`}
-      />
-
-      <Separator />
-
-      <Title>Textarea</Title>
-      <Preview>
-        <label className="label">
-          <span className="label-text">Message</span>
-          <textarea placeholder="Dolor sit amet..." />
-        </label>
-      </Preview>
-      <CodeBlock
-        language="html"
-        code={`<label className="label">
+</label>
+<label className="label">
   <span className="label-text">Message</span>
   <textarea placeholder="Dolor sit amet..." />
-</label>`}
-      />
-
-      <Separator />
-
-      <Title>Select</Title>
-      <Preview>
-        <label className="label">
-          <span className="label-text">Choose a color</span>
-          <select>
-            <option value="#ff0000" selected>
-              Red
-            </option>
-            <option value="#00ff00">Green</option>
-            <option value="#0000ff">Blue</option>
-          </select>
-        </label>
-      </Preview>
-      <CodeBlock
-        language="html"
-        code={`<label className="label">
+</label>
+<label className="label">
   <span className="label-text">Choose a color</span>
   <select>
     <option value="#ff0000" selected>
@@ -63,59 +74,28 @@ export default function Forms() {
     <option value="#00ff00">Green</option>
     <option value="#0000ff">Blue</option>
   </select>
-</label>`}
-      />
-
-      <Separator />
-
-      <Title>Radio button</Title>
-      <Preview>
-        <span className="label-text">Choose a color</span>
-        <label className="label-radio">
-          <input type="radio" name="color-radio" />
-          <span>Red</span>
-        </label>
-        <label className="label-radio">
-          <input type="radio" name="color-radio" />
-          <span>Green</span>
-        </label>
-        <label className="label-radio">
-          <input type="radio" name="color-radio" />
-          <span>Blue</span>
-        </label>
-      </Preview>
-      <CodeBlock
-        code={`<span className="label-text">Choose a color</span>
-<label className="label-radio">
-  <input type="radio" name="color-radio" />
-  <span>Red</span>
 </label>
-<label className="label-radio">
-  <input type="radio" name="color-radio" />
-  <span>Green</span>
-</label>
-<label className="label-radio">
-  <input type="radio" name="color-radio" />
-  <span>Blue</span>
-</label>`}
-      />
-
-      <Separator />
-
-      <Title>Error message</Title>
-      <Preview>
-        <label className="label error">
-          <span className="label-text">First name</span>
-          <input type="text" placeholder="John Doe" />
-          <span className="error">Should be at least 1 character long</span>
-        </label>
-      </Preview>
-      <CodeBlock
-        code={`<label className="label error">
+<div>
+  <span className="label-text">Choose a color</span>
+  <label className="label-radio">
+    <input type="radio" name="color-radio" />
+    <span>Red</span>
+  </label>
+  <label className="label-radio">
+    <input type="radio" name="color-radio" />
+    <span>Green</span>
+  </label>
+  <label className="label-radio">
+    <input type="radio" name="color-radio" />
+    <span>Blue</span>
+  </label>
+</div>
+<label className="label error">
   <span className="label-text">First name</span>
   <input type="text" placeholder="John Doe" />
   <span className="error">Should be at least 1 character long</span>
-</label>`}
+</label>
+</form>`}
       />
     </>
   )

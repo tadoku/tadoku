9e3deddaec6838b777d772a54236b62a8f376612
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2022-12-07 20:50:15 +0900

    make navbar mobile-friendly

--- frontend/tadoku-ui/components/Navbar.tsx
+++ frontend/tadoku-ui/components/Navbar.tsx
@@ -8,24 +8,29 @@ function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(' ')
 }
 
-interface Props {
-  navigation: {
+interface NavigationDropDownProps {
+  type: 'dropdown'
+  label: string
+  links: {
     label: string
     href: string
-    current: boolean
+    IconComponent?: ComponentType<any>
+    onClick?: () => void
   }[]
-  user?: {
-    label: string
-    links: {
-      label: string
-      href: string
-      IconComponent?: ComponentType<any>
-      onClick?: () => void
-    }[]
-  }
 }
 
-export default function Navbar({ navigation, user }: Props) {
+interface NavigationLinkProps {
+  type: 'link'
+  label: string
+  href: string
+  current: boolean
+}
+
+interface Props {
+  navigation: (NavigationLinkProps | NavigationDropDownProps)[]
+}
+
+export default function Navbar({ navigation }: Props) {
   return (
     <Disclosure as="nav" className="bg-white shadow-md shadow-slate-500/10">
       {({ open }) => (
@@ -34,7 +39,7 @@ export default function Navbar({ navigation, user }: Props) {
             <div className="relative flex h-16 items-center justify-between">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
-                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-secondary hover:bg-secondary hover:text-white focus:outline-none focus:ring-3 focus:ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
                     <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
@@ -49,95 +54,71 @@ export default function Navbar({ navigation, user }: Props) {
                 </div>
                 <div className="hidden sm:ml-6 sm:block">
                   <div className="flex space-x-1 md:space-x-2">
-                    {navigation.map(item => (
-                      <a
-                        key={item.label}
-                        href={item.href}
-                        className={classNames(
-                          item.current
-                            ? 'bg-secondary !text-white hover:bg-secondary/80'
-                            : 'text-secondary hover:bg-secondary hover:text-white',
-                          'reset text-xs px-2 py-1 rounded-sm md:px-3 md:py-2 md:rounded-md md:text-sm font-bold',
-                        )}
-                        aria-current={item.current ? 'page' : undefined}
-                      >
-                        {item.label}
-                      </a>
-                    ))}
+                    {navigation.map(item => {
+                      if (item.type === 'dropdown') {
+                        return <DropDown {...item} />
+                      }
+
+                      if (item.type === 'link') {
+                        return (
+                          <a
+                            key={item.label}
+                            href={item.href}
+                            className={classNames(
+                              item.current
+                                ? 'bg-secondary !text-white hover:bg-secondary/80'
+                                : 'text-secondary hover:bg-secondary hover:text-white',
+                              'reset text-xs px-2 py-1 rounded-sm md:px-3 md:py-2 md:rounded-md md:text-sm font-bold inline-flex items-center justify-center',
+                            )}
+                            aria-current={item.current ? 'page' : undefined}
+                          >
+                            {item.label}
+                          </a>
+                        )
+                      }
+                    })}
                   </div>
                 </div>
               </div>
-              {user && (
-                <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:pr-0">
-                  {/* Profile dropdown */}
-                  <Menu as="div" className="relative">
-                    <div>
-                      <Menu.Button className="flex text-secondary hover:bg-secondary hover:text-white px-3 py-2 rounded-md text-xs md:text-sm font-bold">
-                        <span className="sr-only">Open user menu</span>
-                        {user.label}
-                        <ChevronDownIcon
-                          className="ml-2 h-4 w-3 md:h-5 md:w-4"
-                          aria-hidden="true"
-                        />
-                      </Menu.Button>
-                    </div>
-                    <Transition
-                      as={Fragment}
-                      enter="transition ease-out duration-100"
-                      enterFrom="transform opacity-0 scale-95"
-                      enterTo="transform opacity-100 scale-100"
-                      leave="transition ease-in duration-75"
-                      leaveFrom="transform opacity-100 scale-100"
-                      leaveTo="transform opacity-0 scale-95"
-                    >
-                      <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right bg-white py-1 shadow-md shadow-slate-500/20 ring-1 ring-secondary ring-opacity-5 focus:outline-none">
-                        {user.links.map(
-                          ({ label, href, IconComponent, onClick }) => (
-                            <Menu.Item>
-                              {({ active }) => (
-                                <a
-                                  href={href}
-                                  onClick={onClick}
-                                  className={classNames(
-                                    active ? 'bg-gray-100' : '',
-                                    'block px-3 py-2 text-sm text-gray-700 flex font-bold',
-                                  )}
-                                >
-                                  {IconComponent && (
-                                    <IconComponent className="w-5 h-5 mr-3" />
-                                  )}{' '}
-                                  {label}
-                                </a>
-                              )}
-                            </Menu.Item>
-                          ),
-                        )}
-                      </Menu.Items>
-                    </Transition>
-                  </Menu>
-                </div>
-              )}
             </div>
           </div>
 
           <Disclosure.Panel className="sm:hidden">
             <div className="space-y-1 px-2 pt-2 pb-3">
-              {navigation.map(item => (
-                <Disclosure.Button
-                  key={item.label}
-                  as="a"
-                  href={item.href}
-                  className={classNames(
-                    item.current
-                      ? 'bg-gray-900 text-white'
-                      : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                    'block px-3 py-2 rounded-md text-base font-medium',
-                  )}
-                  aria-current={item.current ? 'page' : undefined}
-                >
-                  {item.label}
-                </Disclosure.Button>
-              ))}
+              {navigation.map(item => {
+                if (item.type === 'dropdown') {
+                  return (
+                    <div
+                      key={item.label}
+                      className={classNames(
+                        'text-secondary hover:bg-secondary hover:text-white',
+                        'reset block px-3 py-2 rounded-md text-base font-bold',
+                      )}
+                    >
+                      <DropDownMobile {...item} />
+                    </div>
+                  )
+                }
+
+                if (item.type === 'link') {
+                  return (
+                    <Disclosure.Button
+                      key={item.label}
+                      as="a"
+                      href={item.href}
+                      className={classNames(
+                        item.current
+                          ? 'bg-secondary text-white'
+                          : 'text-secondary hover:bg-secondary hover:text-white',
+                        'reset block px-3 py-2 rounded-md text-base font-bold',
+                      )}
+                      aria-current={item.current ? 'page' : undefined}
+                    >
+                      {item.label}
+                    </Disclosure.Button>
+                  )
+                }
+              })}
             </div>
           </Disclosure.Panel>
         </>
@@ -145,3 +126,97 @@ export default function Navbar({ navigation, user }: Props) {
     </Disclosure>
   )
 }
+
+const DropDown = ({ label, links }: NavigationDropDownProps) => (
+  <div className="">
+    <Menu as="div" className="relative">
+      <div>
+        <Menu.Button className="text-secondary hover:bg-secondary hover:text-white text-xs px-2 py-1 rounded-sm md:px-3 md:py-2 md:rounded-md md:text-sm font-bold flex items-center justify-center">
+          <span className="sr-only">Open navigation menu</span>
+          {label}
+          <ChevronDownIcon
+            className="ml-2 h-4 w-3 md:h-5 md:w-4"
+            aria-hidden="true"
+          />
+        </Menu.Button>
+      </div>
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right bg-white py-1 shadow-md shadow-slate-500/20 ring-1 ring-secondary ring-opacity-5 focus:outline-none">
+          {links.map(({ label, href, IconComponent, onClick }) => (
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  href={href}
+                  onClick={onClick}
+                  className={classNames(
+                    active ? 'bg-gray-100' : '',
+                    'block px-3 py-2 text-sm text-gray-700 flex font-bold',
+                  )}
+                >
+                  {IconComponent && <IconComponent className="w-5 h-5 mr-3" />}{' '}
+                  {label}
+                </a>
+              )}
+            </Menu.Item>
+          ))}
+        </Menu.Items>
+      </Transition>
+    </Menu>
+  </div>
+)
+
+const DropDownMobile = ({ label, links }: NavigationDropDownProps) => (
+  <div className="">
+    {/* Profile dropdown */}
+    <Menu as="div" className="relative">
+      <div>
+        <Menu.Button className="flex items-center justify-between w-full">
+          <span className="sr-only">Open navigation menu</span>
+          {label}
+          <ChevronDownIcon
+            className="ml-2 h-4 w-3 md:h-5 md:w-4"
+            aria-hidden="true"
+          />
+        </Menu.Button>
+      </div>
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right bg-white py-1 shadow-md shadow-slate-500/20 ring-1 ring-secondary ring-opacity-5 focus:outline-none">
+          {links.map(({ label, href, IconComponent, onClick }) => (
+            <Menu.Item>
+              {({ active }) => (
+                <Disclosure.Button
+                  as="a"
+                  href={href}
+                  onClick={onClick}
+                  className={classNames(
+                    active ? 'bg-gray-100' : '',
+                    'block px-3 py-2 text-sm text-gray-700 flex font-bold',
+                  )}
+                >
+                  {IconComponent && <IconComponent className="w-5 h-5 mr-3" />}{' '}
+                  {label}
+                </Disclosure.Button>
+              )}
+            </Menu.Item>
+          ))}
+        </Menu.Items>
+      </Transition>
+    </Menu>
+  </div>
+)
--- frontend/tadoku-ui/pages/navigation.tsx
+++ frontend/tadoku-ui/pages/navigation.tsx
@@ -1,4 +1,4 @@
-import { CodeBlock, Preview, Separator, Title } from '@components/example'
+import { CodeBlock, Preview, Title } from '@components/example'
 import Navbar from '@components/Navbar'
 import {
   ArrowRightOnRectangleIcon,
@@ -6,42 +6,74 @@ import {
   UserIcon,
   WrenchScrewdriverIcon,
 } from '@heroicons/react/24/solid'
-import { toast } from 'react-toastify'
 
 export default function Toasts() {
   return (
     <>
       <h1 className="title mb-8">Toasts</h1>
       <Title>Navigation bar</Title>
-      <Preview className="!bg-neutral-100 !p-0">
+      <Preview className="!bg-neutral-100">
         <Navbar
           navigation={[
-            { label: 'Home', href: '#', current: true },
-            { label: 'Blog', href: '#', current: false },
-            { label: 'Ranking', href: '#', current: false },
-            { label: 'Manual', href: '#', current: false },
-            { label: 'Forum', href: '#', current: false },
+            { type: 'link', label: 'Home', href: '#', current: true },
+            { type: 'link', label: 'Blog', href: '#', current: false },
+            { type: 'link', label: 'Ranking', href: '#', current: false },
+            { type: 'link', label: 'Manual', href: '#', current: false },
+            { type: 'link', label: 'Forum', href: '#', current: false },
+            {
+              type: 'dropdown',
+              label: 'John Doe',
+              links: [
+                {
+                  label: 'Admin',
+                  href: '#',
+                  IconComponent: WrenchScrewdriverIcon,
+                },
+                { label: 'Settings', href: '#', IconComponent: Cog8ToothIcon },
+                { label: 'Profile', href: '#', IconComponent: UserIcon },
+                {
+                  label: 'Log out',
+                  href: '#',
+                  IconComponent: ArrowRightOnRectangleIcon,
+                },
+              ],
+            },
           ]}
-          user={{
-            label: 'John Doe',
-            links: [
-              {
-                label: 'Admin',
-                href: '#',
-                IconComponent: WrenchScrewdriverIcon,
-              },
-              { label: 'Settings', href: '#', IconComponent: Cog8ToothIcon },
-              { label: 'Profile', href: '#', IconComponent: UserIcon },
-              {
-                label: 'Log out',
-                href: '#',
-                IconComponent: ArrowRightOnRectangleIcon,
-              },
-            ],
-          }}
         />
       </Preview>
-      <CodeBlock code={``} />
+      <CodeBlock
+        code={`import Navbar from '@components/Navbar'
+
+const NavigationExample = () => (
+  <Navbar
+    navigation={[
+      { type: 'link', label: 'Home', href: '#', current: true },
+      { type: 'link', label: 'Blog', href: '#', current: false },
+      { type: 'link', label: 'Ranking', href: '#', current: false },
+      { type: 'link', label: 'Manual', href: '#', current: false },
+      { type: 'link', label: 'Forum', href: '#', current: false },
+      {
+        type: 'dropdown',
+        label: 'John Doe',
+        links: [
+          {
+            label: 'Admin',
+            href: '#',
+            IconComponent: WrenchScrewdriverIcon,
+          },
+          { label: 'Settings', href: '#', IconComponent: Cog8ToothIcon },
+          { label: 'Profile', href: '#', IconComponent: UserIcon },
+          {
+            label: 'Log out',
+            href: '#',
+            IconComponent: ArrowRightOnRectangleIcon,
+          },
+        ],
+      },
+    ]}
+  />
+)`}
+      />
     </>
   )
 }

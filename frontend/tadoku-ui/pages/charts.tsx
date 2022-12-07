f7911996baa58c1829da8bdebe8f9505407982e8
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2022-12-07 23:21:43 +0900

    add reading activity chart implementation

--- frontend/tadoku-ui/pages/charts.tsx
+++ frontend/tadoku-ui/pages/charts.tsx
@@ -1,14 +1,174 @@
-import { datasetDefaults } from '@components/charts'
+import { chartColors, datasetDefaults } from '@components/charts'
 import { CodeBlock, Preview, Separator, Title } from '@components/example'
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
+import {
+  Chart as ChartJS,
+  ArcElement,
+  Tooltip,
+  Legend,
+  LinearScale,
+  CategoryScale,
+  BarElement,
+  PointElement,
+  LineElement,
+  LineController,
+  BarController,
+  TimeScale,
+} from 'chart.js'
+import { Chart } from 'react-chartjs-2'
 import { Doughnut } from 'react-chartjs-2'
+import 'chartjs-adapter-luxon'
+import { faker } from '@faker-js/faker'
 
-ChartJS.register(ArcElement, Tooltip, Legend)
+ChartJS.register(
+  ArcElement,
+  Tooltip,
+  Legend,
+  LinearScale,
+  CategoryScale,
+  BarElement,
+  PointElement,
+  LineElement,
+  LineController,
+  BarController,
+  TimeScale,
+)
 
 export default function Typography() {
   return (
     <>
       <h1 className="title mb-8">Charts</h1>
+
+      <Title>Reading activity chart</Title>
+      <Preview>
+        <div className="max-w-[900px]">
+          <ReadingActivityChart />
+        </div>
+      </Preview>
+      <CodeBlock
+        language="html"
+        code={`import { chartColors, datasetDefaults } from '@components/charts'
+import {
+  Chart as ChartJS,
+  Tooltip,
+  Legend,
+  LinearScale,
+  CategoryScale,
+  BarElement,
+  PointElement,
+  LineElement,
+  LineController,
+  BarController,
+  TimeScale,
+} from 'chart.js'
+import { Chart } from 'react-chartjs-2'
+import 'chartjs-adapter-luxon'
+import { faker } from '@faker-js/faker'
+
+ChartJS.register(
+  Tooltip,
+  Legend,
+  LinearScale,
+  CategoryScale,
+  BarElement,
+  PointElement,
+  LineElement,
+  LineController,
+  BarController,
+  TimeScale,
+)
+
+function ReadingActivityChart() {
+  const labels = Array.from(Array(14).keys()).map(
+    day => '2022-12-' + (day + 1).toString().padStart(2, '0'),
+  )
+
+  const comicData = labels.map(() =>
+    faker.datatype.number({ min: 0, max: 1000 }),
+  )
+  const bookData = labels.map(() =>
+    faker.datatype.number({ min: 0, max: 1000 }),
+  )
+  const cumulativeScore = comicData
+    .map((comic, i) => comic + bookData[i])
+    .reduce((acc, val) => {
+      if (acc.length > 0) {
+        val += acc[acc.length - 1]
+      }
+      acc.push(val)
+      return acc
+    }, [] as number[])
+
+  return (
+    <Chart
+      type="bar"
+      data={{
+        labels,
+        datasets: [
+          {
+            type: 'line' as const,
+            label: 'Cumulative Score',
+            borderColor: chartColors[chartColors.length - 1],
+            borderWidth: 2,
+            fill: false,
+            data: cumulativeScore,
+            yAxisID: 'yCumulative',
+          },
+          {
+            type: 'bar' as const,
+            label: 'Comic',
+            backgroundColor: chartColors[0],
+            data: comicData,
+            yAxisID: 'yScore',
+          },
+          {
+            type: 'bar' as const,
+            label: 'Book',
+            backgroundColor: chartColors[1],
+            data: bookData,
+            yAxisID: 'yScore',
+          },
+        ],
+      }}
+      options={{
+        scales: {
+          x: {
+            stacked: true,
+            type: 'time',
+            time: {
+              tooltipFormat: 'MMMM d',
+              unit: 'day',
+              displayFormats: {
+                day: 'MMM dd',
+              },
+            },
+          },
+          yScore: {
+            title: {
+              text: 'Score',
+              display: true,
+            },
+            stacked: true,
+            position: 'left',
+          },
+          yCumulative: {
+            title: {
+              text: 'Cumulative score',
+              display: true,
+            },
+            position: 'right',
+            grid: {
+              drawOnChartArea: false,
+            },
+          },
+        },
+      }}
+    />
+  )
+}`}
+      />
+
+      <Separator />
+
       <Title>Doughnut chart</Title>
 
       <div className="h-stack w-full">
@@ -56,5 +216,94 @@ const Example = () => (
         </div>
       </div>
     </>
+  )
+}
+
+function ReadingActivityChart() {
+  const labels = Array.from(Array(14).keys()).map(
+    day => '2022-12-' + (day + 1).toString().padStart(2, '0'),
+  )
+
+  const comicData = labels.map(() =>
+    faker.datatype.number({ min: 0, max: 1000 }),
+  )
+  const bookData = labels.map(() =>
+    faker.datatype.number({ min: 0, max: 1000 }),
+  )
+  const cumulativeScore = comicData
+    .map((comic, i) => comic + bookData[i])
+    .reduce((acc, val) => {
+      if (acc.length > 0) {
+        val += acc[acc.length - 1]
+      }
+      acc.push(val)
+      return acc
+    }, [] as number[])
+
+  return (
+    <Chart
+      type="bar"
+      data={{
+        labels,
+        datasets: [
+          {
+            type: 'line' as const,
+            label: 'Cumulative Score',
+            borderColor: chartColors[chartColors.length - 1],
+            borderWidth: 2,
+            fill: false,
+            data: cumulativeScore,
+            yAxisID: 'yCumulative',
+          },
+          {
+            type: 'bar' as const,
+            label: 'Comic',
+            backgroundColor: chartColors[0],
+            data: comicData,
+            yAxisID: 'yScore',
+          },
+          {
+            type: 'bar' as const,
+            label: 'Book',
+            backgroundColor: chartColors[1],
+            data: bookData,
+            yAxisID: 'yScore',
+          },
+        ],
+      }}
+      options={{
+        scales: {
+          x: {
+            stacked: true,
+            type: 'time',
+            time: {
+              tooltipFormat: 'MMMM d',
+              unit: 'day',
+              displayFormats: {
+                day: 'MMM dd',
+              },
+            },
+          },
+          yScore: {
+            title: {
+              text: 'Score',
+              display: true,
+            },
+            stacked: true,
+            position: 'left',
+          },
+          yCumulative: {
+            title: {
+              text: 'Cumulative score',
+              display: true,
+            },
+            position: 'right',
+            grid: {
+              drawOnChartArea: false,
+            },
+          },
+        },
+      }}
+    />
   )
 }

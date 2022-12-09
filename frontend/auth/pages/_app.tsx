e8b6af5ef7a8c39cdfdc31b28170b48250a8ba73
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2022-12-09 20:49:07 +0900

    add heroicons and headlessui

--- frontend/auth/pages/_app.tsx
+++ frontend/auth/pages/_app.tsx
@@ -3,9 +3,15 @@ import ory from '../src/ory'
 import { Atom, Provider } from 'jotai'
 import { AppContextWithSession, sessionAtom } from '../src/session'
 import { Session } from '@ory/client'
-import Header from '../ui/Header'
 import ToastContainer from 'tadoku-ui/components/toasts'
 import 'tadoku-ui/styles/globals.css'
+import Navbar from 'tadoku-ui/components/Navbar'
+import {
+  ArrowRightOnRectangleIcon,
+  Cog8ToothIcon,
+  UserIcon,
+  WrenchScrewdriverIcon,
+} from '@heroicons/react/20/solid'
 
 interface Props {
   session: Session | undefined
@@ -29,8 +35,33 @@ const MyApp = ({ Component, pageProps }: AppProps<Props>) => {
   return (
     <Provider initialValues={getInitialValues()}>
       <div>
-        <Header />
-        <hr />
+        <Navbar
+          navigation={[
+            { type: 'link', label: 'Home', href: '#', current: true },
+            { type: 'link', label: 'Blog', href: '#', current: false },
+            { type: 'link', label: 'Ranking', href: '#', current: false },
+            { type: 'link', label: 'Manual', href: '#', current: false },
+            { type: 'link', label: 'Forum', href: '#', current: false },
+            {
+              type: 'dropdown',
+              label: 'John Doe',
+              links: [
+                {
+                  label: 'Admin',
+                  href: '#',
+                  IconComponent: WrenchScrewdriverIcon,
+                },
+                { label: 'Settings', href: '#', IconComponent: Cog8ToothIcon },
+                { label: 'Profile', href: '#', IconComponent: UserIcon },
+                {
+                  label: 'Log out',
+                  href: '#',
+                  IconComponent: ArrowRightOnRectangleIcon,
+                },
+              ],
+            },
+          ]}
+        />
         <Component {...pageProps} />
         <ToastContainer />
       </div>

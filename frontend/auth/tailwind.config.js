00e8e96dc3990b721e383a9c9417b61c2d3ff11d
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2022-12-09 20:49:20 +0900

    fix tailwind config

--- frontend/auth/tailwind.config.js
+++ frontend/auth/tailwind.config.js
@@ -1,4 +1,4 @@
 const config = require('tadoku-ui/tailwind.config.js')
-config.content.push('./node_modules/tadoku-ui/src/*.{js,ts,jsx,tsx}')
+config.content.push('../node_modules/tadoku-ui/components/*.{js,ts,jsx,tsx}')
 
 module.exports = config

ed541cefe21603e868f76bbda30a0c3f7452dea3
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2022-12-26 14:38:33 +0900

    update next and react-table to fix swc incompatible production builds

--- frontend/apps/styleguide/next.config.js
+++ frontend/apps/styleguide/next.config.js
@@ -2,8 +2,7 @@
 const nextConfig = {
   output: 'standalone',
   reactStrictMode: true,
-  // Needs to be disabled until swc is fixed
-  swcMinify: false,
+  swcMinify: true,
 }
 
 const withTM = require('next-transpile-modules')(['ui']);

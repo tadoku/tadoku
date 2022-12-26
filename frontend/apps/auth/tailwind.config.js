345efa9f841437c0e1a2ba48bb3dd9e1be25999d
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2022-12-26 16:26:50 +0900

    use pnpm in dev env

--- frontend/apps/auth/tailwind.config.js
+++ frontend/apps/auth/tailwind.config.js
@@ -1,4 +1,4 @@
 const config = require('ui/tailwind.config.js')
-config.content.push('../node_modules/ui/components/*.{js,ts,jsx,tsx}')
+config.content.push('./node_modules/ui/components/*.{js,ts,jsx,tsx}')
 
 module.exports = config

4561569e8dcf9ff51882b3b73674690bbb46653f
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2022-12-27 21:10:05 +0900

    navbar tweaks

--- frontend/packages/ui/components/Navbar.tsx
+++ frontend/packages/ui/components/Navbar.tsx
@@ -35,100 +35,111 @@ export function Navbar({ navigation, logoHref, width = 'max-w-7xl' }: Props) {
   const router = useRouter()
 
   return (
-    <Disclosure as="nav" className="bg-white shadow-md shadow-slate-500/10">
-      {({ open }) => (
-        <>
-          <div className={`mx-auto ${width} px-2 sm:px-6 lg:px-8`}>
-            <div className="relative flex h-16 items-center justify-between">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                {/* Mobile menu button*/}
-                <Disclosure.Button className="inline-flex items-center justify-center p-2 text-secondary hover:bg-secondary hover:text-white focus:outline-none focus:ring-3 focus:ring-inset focus:ring-white">
-                  <span className="sr-only">Open main menu</span>
-                  {open ? (
-                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-                  ) : (
-                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-                  )}
-                </Disclosure.Button>
-              </div>
-              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-between">
-                <div className="flex flex-shrink-0 items-center">
-                  <Link href={logoHref}>
-                    <Logo scale={0.8} />
-                  </Link>
+    <>
+      <div className="h-10 sm:h-16 absolute top-0 left-0 right-0 bg-white z-0"></div>
+      <Disclosure
+        as="nav"
+        className="bg-white sticky top-0 z-50 backdrop-blur bg-white/70 border-b border-black/10 shadow shadow-slate-500/10"
+      >
+        {({ open }) => (
+          <>
+            <div className={`mx-auto ${width} px-2 sm:px-6 lg:px-8 z-10`}>
+              <div className="relative flex h-10 sm:h-16 items-center justify-between">
+                <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                  {/* Mobile menu button*/}
+                  <Disclosure.Button className="inline-flex items-center justify-center p-2 text-secondary hover:bg-secondary hover:text-white focus:outline-none focus:ring-3 focus:ring-inset focus:ring-white">
+                    <span className="sr-only">Open main menu</span>
+                    {open ? (
+                      <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                    ) : (
+                      <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                    )}
+                  </Disclosure.Button>
                 </div>
-                <div className="hidden sm:ml-6 sm:block">
-                  <div className="flex space-x-1 md:space-x-2">
-                    {navigation.map(item => {
-                      if (item.type === 'dropdown') {
-                        return <DropDown {...item} key={item.label} />
-                      }
+                <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-between">
+                  <div className="flex flex-shrink-0 items-center">
+                    <Link href={logoHref}>
+                      <span className="hidden sm:block">
+                        <Logo scale={0.8} />
+                      </span>
+                      <span className="block sm:hidden">
+                        <Logo scale={0.5} />
+                      </span>
+                    </Link>
+                  </div>
+                  <div className="hidden sm:ml-6 sm:block">
+                    <div className="flex space-x-1 md:space-x-2">
+                      {navigation.map(item => {
+                        if (item.type === 'dropdown') {
+                          return <DropDown {...item} key={item.label} />
+                        }
 
-                      if (item.type === 'link') {
-                        return (
-                          <Link
-                            key={item.label}
-                            href={item.href}
-                            className={classNames(
-                              item.current || router.pathname === item.href
-                                ? 'bg-secondary !text-white hover:bg-secondary/80'
-                                : 'text-secondary hover:bg-secondary hover:text-white',
-                              'reset text-xs px-2 py-1 md:px-3 md:py-2 md:text-sm font-bold inline-flex items-center justify-center',
-                            )}
-                            aria-current={item.current ? 'page' : undefined}
-                          >
-                            {item.label}
-                          </Link>
-                        )
-                      }
-                    })}
+                        if (item.type === 'link') {
+                          return (
+                            <Link
+                              key={item.label}
+                              href={item.href}
+                              className={classNames(
+                                item.current || router.pathname === item.href
+                                  ? 'bg-secondary !text-white hover:bg-secondary/80'
+                                  : 'text-secondary hover:bg-secondary hover:text-white',
+                                'reset text-xs px-2 py-1 md:px-3 md:py-2 md:text-sm font-bold inline-flex items-center justify-center',
+                              )}
+                              aria-current={item.current ? 'page' : undefined}
+                            >
+                              {item.label}
+                            </Link>
+                          )
+                        }
+                      })}
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
 
-          <Disclosure.Panel className="sm:hidden">
-            <div className="space-y-1 px-2 pt-2 pb-3">
-              {navigation.map(item => {
-                if (item.type === 'dropdown') {
-                  return (
-                    <div
-                      key={item.label}
-                      className={classNames(
-                        'text-secondary hover:bg-secondary hover:text-white',
-                        'reset block px-3 py-2 text-base font-bold',
-                      )}
-                    >
-                      <DropDownMobile {...item} />
-                    </div>
-                  )
-                }
+            <Disclosure.Panel className="sm:hidden">
+              <div className="space-y-1 px-2 pt-2 pb-3">
+                {navigation.map(item => {
+                  if (item.type === 'dropdown') {
+                    return (
+                      <div
+                        key={item.label}
+                        className={classNames(
+                          'text-secondary hover:bg-secondary hover:text-white',
+                          'reset block px-3 py-2 text-base font-bold',
+                        )}
+                      >
+                        <DropDownMobile {...item} />
+                      </div>
+                    )
+                  }
 
-                if (item.type === 'link') {
-                  return (
-                    <Disclosure.Button
-                      key={item.label}
-                      as={Link}
-                      href={item.href}
-                      className={classNames(
-                        item.current
-                          ? 'bg-secondary text-white'
-                          : 'text-secondary hover:bg-secondary hover:text-white',
-                        'reset transition-[background-color] block px-3 py-2 text-base font-bold',
-                      )}
-                      aria-current={item.current ? 'page' : undefined}
-                    >
-                      {item.label}
-                    </Disclosure.Button>
-                  )
-                }
-              })}
-            </div>
-          </Disclosure.Panel>
-        </>
-      )}
-    </Disclosure>
+                  if (item.type === 'link') {
+                    return (
+                      <Disclosure.Button
+                        key={item.label}
+                        as={Link}
+                        href={item.href}
+                        className={classNames(
+                          item.current
+                            ? 'bg-secondary text-white'
+                            : 'text-secondary hover:bg-secondary hover:text-white',
+                          'reset transition-[background-color] block px-3 py-2 text-base font-bold',
+                        )}
+                        aria-current={item.current ? 'page' : undefined}
+                      >
+                        {item.label}
+                      </Disclosure.Button>
+                    )
+                  }
+                })}
+              </div>
+            </Disclosure.Panel>
+          </>
+        )}
+      </Disclosure>
+    </>
   )
 }
 

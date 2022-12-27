45d2eb1afe2d49546a4f98d5e03a1789beb58447
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2022-12-27 21:18:57 +0900

    styleguide: fix preview of navbar

--- frontend/apps/styleguide/pages/navigation.tsx
+++ frontend/apps/styleguide/pages/navigation.tsx
@@ -10,37 +10,43 @@ import {
 export default function Toasts() {
   return (
     <>
-      <h1 className="title mb-8">Toasts</h1>
-      <Title>Navigation bar</Title>
+      <h1 className="title mb-8">Navigation</h1>
+      <Title>Example</Title>
       <Preview className="!bg-neutral-100">
-        <Navbar
-          logoHref="/"
-          navigation={[
-            { type: 'link', label: 'Home', href: '#', current: true },
-            { type: 'link', label: 'Blog', href: '#', current: false },
-            { type: 'link', label: 'Ranking', href: '#', current: false },
-            { type: 'link', label: 'Manual', href: '#', current: false },
-            { type: 'link', label: 'Forum', href: '#', current: false },
-            {
-              type: 'dropdown',
-              label: 'John Doe',
-              links: [
-                {
-                  label: 'Admin',
-                  href: '#',
-                  IconComponent: WrenchScrewdriverIcon,
-                },
-                { label: 'Settings', href: '#', IconComponent: Cog8ToothIcon },
-                { label: 'Profile', href: '#', IconComponent: UserIcon },
-                {
-                  label: 'Log out',
-                  href: '#',
-                  IconComponent: ArrowRightOnRectangleIcon,
-                },
-              ],
-            },
-          ]}
-        />
+        <div className="relative">
+          <Navbar
+            logoHref="/"
+            navigation={[
+              { type: 'link', label: 'Home', href: '#', current: true },
+              { type: 'link', label: 'Blog', href: '#', current: false },
+              { type: 'link', label: 'Ranking', href: '#', current: false },
+              { type: 'link', label: 'Manual', href: '#', current: false },
+              { type: 'link', label: 'Forum', href: '#', current: false },
+              {
+                type: 'dropdown',
+                label: 'John Doe',
+                links: [
+                  {
+                    label: 'Admin',
+                    href: '#',
+                    IconComponent: WrenchScrewdriverIcon,
+                  },
+                  {
+                    label: 'Settings',
+                    href: '#',
+                    IconComponent: Cog8ToothIcon,
+                  },
+                  { label: 'Profile', href: '#', IconComponent: UserIcon },
+                  {
+                    label: 'Log out',
+                    href: '#',
+                    IconComponent: ArrowRightOnRectangleIcon,
+                  },
+                ],
+              },
+            ]}
+          />
+        </div>
       </Preview>
       <CodeBlock
         code={`import { Navbar } from 'ui'

93b580c6c8378a2004baee644de75061407b7706
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2022-12-30 19:20:27 +0900

    ui: make breadcrumb responsive
    fixes #516

--- frontend/packages/ui/components/Breadcrumb.tsx
+++ frontend/packages/ui/components/Breadcrumb.tsx
@@ -1,36 +1,33 @@
-import { ChevronRightIcon } from '@heroicons/react/20/solid'
+import { ArrowUturnLeftIcon, ChevronRightIcon } from '@heroicons/react/20/solid'
 import classNames from 'classnames'
 import { ComponentType } from 'react'
 
 interface Props {
-  links: {
-    label: string
-    href: string
-    IconComponent?: ComponentType<any>
-  }[]
+  links: Link[]
+}
+
+interface Link {
+  label: string
+  href: string
+  IconComponent?: ComponentType<any>
 }
 
 export function Breadcrumb({ links }: Props) {
   return (
     <nav className="flex" aria-label="Breadcrumb">
-      <ol className="inline-flex items-center space-x-1 md:space-x-3">
-        {links.map(({ label, href, IconComponent }, i) => (
+      {links.length > 1 ? (
+        <div className="md:hidden">
+          <BreadcrumbLink
+            {...links[links.length - 2]}
+            IconComponent={ArrowUturnLeftIcon}
+            isLast={false}
+          />
+        </div>
+      ) : null}
+      <ol className="hidden items-center space-x-1 md:space-x-3 md:inline-flex">
+        {links.map((link, i) => (
           <li className="inline-flex items-center" key={i}>
-            <a
-              href={href}
-              className={classNames(
-                'reset inline-flex items-center text-sm font-medium',
-                {
-                  'text-gray-800 hover:text-primary': i !== links.length - 1,
-                  'text-gray-500 pointer-events-none': i === links.length - 1,
-                },
-              )}
-            >
-              {IconComponent ? (
-                <IconComponent className="w-4 h-4 mr-2" />
-              ) : null}
-              {label}
-            </a>
+            <BreadcrumbLink {...link} isLast={i === links.length - 1} />
             {i !== links.length - 1 ? (
               <ChevronRightIcon className="w-4 h-4 ml-3" />
             ) : null}
@@ -40,3 +37,28 @@ export function Breadcrumb({ links }: Props) {
     </nav>
   )
 }
+
+interface BreadcrumbLinkProps extends Link {
+  isLast: boolean
+}
+
+const BreadcrumbLink = ({
+  href,
+  IconComponent,
+  label,
+  isLast,
+}: BreadcrumbLinkProps) => (
+  <a
+    href={href}
+    className={classNames(
+      'reset inline-flex items-center text-sm font-medium',
+      {
+        'text-gray-800 hover:text-primary': !isLast,
+        'text-gray-500 pointer-events-none': isLast,
+      },
+    )}
+  >
+    {IconComponent ? <IconComponent className="w-4 h-4 mr-2" /> : null}
+    {label}
+  </a>
+)

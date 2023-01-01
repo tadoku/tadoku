943f749eaf46bea1da76639d2f3397665299318e
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2023-01-01 17:33:35 +0900

    webv2: add my contests list

--- frontend/apps/webv2/app/common/session.ts
+++ frontend/apps/webv2/app/common/session.ts
@@ -6,11 +6,33 @@ import { AxiosError } from 'axios'
 import ory from '@app/common/ory'
 import { AppContext } from 'next/app'
 import { NextPageContext } from 'next'
+import getConfig from 'next/config'
+import { useCurrentLocation } from '@app/common/hooks'
+
+const { publicRuntimeConfig } = getConfig()
 
 export const sessionAtom = atom(undefined as undefined | Session)
 
 export const useSession = () => {
   return useAtom(sessionAtom)
+}
+
+export const useSessionOrRedirect = () => {
+  const result = useAtom(sessionAtom)
+  const currentUrl = useCurrentLocation()
+  const router = useRouter()
+
+  const [session] = result
+
+  useEffect(() => {
+    if (!session) {
+      router.push(
+        publicRuntimeConfig.authUiUrl + `/login?return_to=${currentUrl}`,
+      )
+    }
+  })
+
+  return result
 }
 
 export const logoutTokenAtom = atom(undefined as undefined | string)

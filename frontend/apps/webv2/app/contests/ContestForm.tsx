036dd24e258730ebae2ee6994bf41bfdd1660355
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2023-01-03 23:17:00 +0900

    webv2: fix contestform

--- frontend/apps/webv2/app/contests/ContestForm.tsx
+++ frontend/apps/webv2/app/contests/ContestForm.tsx
@@ -1,5 +1,5 @@
 import { AutocompleteMultiInput, Input, Checkbox } from 'ui'
-import { useForm } from 'react-hook-form'
+import { FormProvider, useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { z } from 'zod'
 import { date } from '@app/common/regex'
@@ -69,11 +69,16 @@ interface Props {
 export const ContestForm = ({
   configurationOptions: { languages, activities, canCreateOfficialRound },
 }: Props) => {
-  const { register, handleSubmit, formState, control, watch } = useForm({
+  const defaultValues: Partial<ContestFormSchema> = {
+    activityTypeIdAllowList: activities.filter(a => a.default),
+  }
+
+  const methods = useForm({
     resolver: zodResolver(ContestFormSchema),
+    defaultValues,
   })
-  const isOfficial = watch('official')
-  const isPrivate = watch('private')
+  const isOfficial = methods.watch('official')
+  const isPrivate = methods.watch('private')
 
   const router = useRouter()
   const createContestMutation = useCreateContest(id =>
@@ -87,112 +92,83 @@ export const ContestForm = ({
   }
 
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit, errors => console.log(errors))}
-      className="v-stack spaced max-w-screen-sm"
-    >
-      <div className="card">
-        <Input
-          name="description"
-          label="Contest name"
-          register={register}
-          formState={formState}
-          type="text"
-        />
-      </div>
-      <div className="card">
-        <div className="v-stack spaced">
-          <h2 className="subtitle">Schedule</h2>
-          <div className="v-stack md:h-stack fill spaced">
-            <div>
-              <Input
-                name="contestStart"
-                label="Start date"
-                register={register}
-                formState={formState}
-                type="date"
-              />
-            </div>
-            <div>
-              <Input
-                name="contestEnd"
-                label="End date"
-                register={register}
-                formState={formState}
-                type="date"
-              />
-            </div>
-          </div>
-          <Input
-            name="registrationEnd"
-            label="Sign up deadline"
-            register={register}
-            formState={formState}
-            type="date"
-          />
+    <FormProvider {...methods}>
+      <form
+        onSubmit={methods.handleSubmit(onSubmit, errors => console.log(errors))}
+        className="v-stack spaced max-w-screen-sm"
+      >
+        <div className="card">
+          <Input name="description" label="Contest name" type="text" />
         </div>
-      </div>
-      <div className="card v-stack spaced">
-        <h2 className="subtitle">Configuration</h2>
-        <AutocompleteMultiInput
-          name="languageCodeAllowList"
-          label="Allowed languages"
-          hint="Leave empty to accept all languages"
-          options={languages}
-          control={control}
-          match={(option, query) =>
-            option.name
-              .toLowerCase()
-              .replace(/[^a-zA-Z0-9]/g, '')
-              .includes(query.toLowerCase())
-          }
-          getIdForOption={option => option.code}
-          format={option => option.name}
-        />
-        <AutocompleteMultiInput
-          name="activityTypeIdAllowList"
-          label="Allowed activities"
-          options={activities}
-          control={control}
-          defaultValue={activities.filter(a => a.default)}
-          match={(option, query) =>
-            option.name
-              .toLowerCase()
-              .replace(/[^a-zA-Z0-9]/g, '')
-              .includes(query.toLowerCase())
-          }
-          getIdForOption={option => option.id}
-          format={option => option.name}
-        />
-        {canCreateOfficialRound && !isPrivate && (
-          <Checkbox
-            name="official"
-            label="Official round"
-            register={register}
-            formState={formState}
+        <div className="card">
+          <div className="v-stack spaced">
+            <h2 className="subtitle">Schedule</h2>
+            <div className="v-stack md:h-stack fill spaced">
+              <div>
+                <Input name="contestStart" label="Start date" type="date" />
+              </div>
+              <div>
+                <Input name="contestEnd" label="End date" type="date" />
+              </div>
+            </div>
+            <Input
+              name="registrationEnd"
+              label="Sign up deadline"
+              type="date"
+            />
+          </div>
+        </div>
+        <div className="card v-stack spaced">
+          <h2 className="subtitle">Configuration</h2>
+          <AutocompleteMultiInput
+            name="languageCodeAllowList"
+            label="Allowed languages"
+            hint="Leave empty to accept all languages"
+            options={languages}
+            match={(option, query) =>
+              option.name
+                .toLowerCase()
+                .replace(/[^a-zA-Z0-9]/g, '')
+                .includes(query.toLowerCase())
+            }
+            getIdForOption={option => option.code}
+            format={option => option.name}
           />
-        )}
-        {!isOfficial && (
-          <Checkbox
-            name="private"
-            label="Only those with link can sign up"
-            register={register}
-            formState={formState}
+          <AutocompleteMultiInput
+            name="activityTypeIdAllowList"
+            label="Allowed activities"
+            options={activities}
+            match={(option, query) =>
+              option.name
+                .toLowerCase()
+                .replace(/[^a-zA-Z0-9]/g, '')
+                .includes(query.toLowerCase())
+            }
+            getIdForOption={option => option.id}
+            format={option => option.name}
           />
-        )}
-      </div>
-      <div className="h-stack spaced justify-end">
-        <a href={routes.contestListOfficial()} className="btn ghost">
-          Cancel
-        </a>
-        <button
-          type="submit"
-          className="btn primary"
-          disabled={formState.isSubmitting}
-        >
-          Create
-        </button>
-      </div>
-    </form>
+          {canCreateOfficialRound && !isPrivate && (
+            <Checkbox name="official" label="Official round" />
+          )}
+          {!isOfficial && (
+            <Checkbox name="private" label="Only those with link can sign up" />
+          )}
+        </div>
+        <div className="h-stack spaced justify-end">
+          <a href={routes.contestListOfficial()} className="btn ghost">
+            Cancel
+          </a>
+          <button
+            type="submit"
+            className="btn primary"
+            disabled={
+              methods.formState.isSubmitting || !methods.formState.isValid
+            }
+          >
+            Create
+          </button>
+        </div>
+      </form>
+    </FormProvider>
   )
 }

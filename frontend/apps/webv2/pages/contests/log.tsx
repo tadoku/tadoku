2ebbe70fa658be86e85ea8f39612f846f3ad4487
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2023-01-07 14:37:20 +0900

    webv2: log page boilerplate

---
+++ frontend/apps/webv2/pages/contests/log.tsx
@@ -0,0 +1,38 @@
+import type { NextPage } from 'next'
+import { Breadcrumb } from 'ui'
+import { HomeIcon } from '@heroicons/react/20/solid'
+import { ContestForm } from '@app/contests/ContestForm'
+import { useContestConfigurationOptions } from '@app/contests/api'
+import { routes } from '@app/common/routes'
+
+interface Props {}
+
+const Page: NextPage<Props> = () => {
+  const options = useContestConfigurationOptions()
+
+  return (
+    <>
+      <div className="pb-4">
+        <Breadcrumb
+          links={[
+            { label: 'Home', href: routes.home(), IconComponent: HomeIcon },
+            { label: 'Contests', href: routes.contestListOfficial() },
+            { label: 'New log', href: routes.contestLog() },
+          ]}
+        />
+      </div>
+      <h1 className="title mb-4">New log</h1>
+      {options.isLoading ? <p>Loading...</p> : null}
+      {options.isError ? (
+        <span className="flash error">
+          Could not load page, please try again later.
+        </span>
+      ) : null}
+      {options.isSuccess ? (
+        <ContestForm configurationOptions={options.data} />
+      ) : null}
+    </>
+  )
+}
+
+export default Page

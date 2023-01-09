a72eb945d0f3640009eab6ce51d535213f285010
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2023-01-09 12:42:25 +0900

    immersion: split contest and log http server

--- services/immersion-api/http/rest/contests.go
+++ services/immersion-api/http/rest/contests.go
@@ -10,7 +10,6 @@ import (
 
 	"github.com/tadoku/tadoku/services/immersion-api/domain/contestcommand"
 	"github.com/tadoku/tadoku/services/immersion-api/domain/contestquery"
-	"github.com/tadoku/tadoku/services/immersion-api/domain/logquery"
 	"github.com/tadoku/tadoku/services/immersion-api/http/rest/openapi"
 )
 
@@ -391,60 +390,6 @@ func (s *Server) ContestFindOngoingRegistrations(ctx echo.Context) error {
 		}
 
 		res.Registrations[i] = registration
-	}
-
-	return ctx.JSON(http.StatusOK, res)
-}
-
-// Fetches the configuration options for a log
-// (GET /logs/configuration-options)
-func (s *Server) LogGetConfigurations(ctx echo.Context) error {
-	opts, err := s.logQueryService.FetchLogConfigurationOptions(ctx.Request().Context())
-	if err != nil {
-		if errors.Is(err, logquery.ErrUnauthorized) {
-			return ctx.NoContent(http.StatusUnauthorized)
-		}
-		return ctx.NoContent(http.StatusInternalServerError)
-	}
-
-	res := openapi.LogConfigurationOptions{
-		Activities: make([]openapi.Activity, len(opts.Activities)),
-		Languages:  make([]openapi.Language, len(opts.Languages)),
-		Units:      make([]openapi.Unit, len(opts.Units)),
-		Tags:       make([]openapi.Tag, len(opts.Tags)),
-	}
-
-	for i, it := range opts.Activities {
-		it := it
-		res.Activities[i] = openapi.Activity{
-			Id:   it.ID,
-			Name: it.Name,
-		}
-	}
-
-	for i, it := range opts.Languages {
-		res.Languages[i] = openapi.Language{
-			Code: it.Code,
-			Name: it.Name,
-		}
-	}
-
-	for i, it := range opts.Units {
-		res.Units[i] = openapi.Unit{
-			Id:            it.ID,
-			LogActivityId: it.LogActivityID,
-			Name:          it.Name,
-			Modifier:      it.Modifier,
-			LanguageCode:  it.LanguageCode,
-		}
-	}
-
-	for i, it := range opts.Tags {
-		res.Tags[i] = openapi.Tag{
-			Id:            it.ID,
-			LogActivityId: it.LogActivityID,
-			Name:          it.Name,
-		}
 	}
 
 	return ctx.JSON(http.StatusOK, res)

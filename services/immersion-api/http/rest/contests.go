acd4f3a04a2e16af24ea042c914d2b333704afaf
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2023-01-11 22:05:07 +0900

    immersion: find latest official http implementation

--- services/immersion-api/http/rest/contests.go
+++ services/immersion-api/http/rest/contests.go
@@ -398,5 +398,50 @@ func (s *Server) ContestFindOngoingRegistrations(ctx echo.Context) error {
 // Fetches the latest official contest
 // (GET /contests/latest-official)
 func (s *Server) ContestFindLatestOfficial(ctx echo.Context) error {
-	return nil
+	contest, err := s.contestQueryService.FindLatestOfficial(ctx.Request().Context())
+	if err != nil {
+		if errors.Is(err, contestquery.ErrNotFound) {
+			return ctx.NoContent(http.StatusNotFound)
+		}
+
+		ctx.Echo().Logger.Error(err)
+		return ctx.NoContent(http.StatusInternalServerError)
+	}
+
+	langs := make([]openapi.Language, len(contest.AllowedLanguages))
+	for i, it := range contest.AllowedLanguages {
+		langs[i] = openapi.Language{
+			Code: it.Code,
+			Name: it.Name,
+		}
+	}
+
+	if len(langs) == 0 {
+		langs = nil
+	}
+
+	acts := make([]openapi.Activity, len(contest.AllowedActivities))
+	for i, it := range contest.AllowedActivities {
+		acts[i] = openapi.Activity{
+			Id:   it.ID,
+			Name: it.Name,
+		}
+	}
+
+	return ctx.JSON(http.StatusOK, openapi.ContestView{
+		Id:                   &contest.ID,
+		ContestStart:         types.Date{Time: contest.ContestStart},
+		ContestEnd:           types.Date{Time: contest.ContestEnd},
+		RegistrationEnd:      types.Date{Time: contest.RegistrationEnd},
+		Description:          contest.Description,
+		OwnerUserId:          &contest.OwnerUserID,
+		OwnerUserDisplayName: &contest.OwnerUserDisplayName,
+		Official:             contest.Official,
+		Private:              contest.Private,
+		AllowedLanguages:     langs,
+		AllowedActivities:    acts,
+		CreatedAt:            &contest.CreatedAt,
+		UpdatedAt:            &contest.UpdatedAt,
+		Deleted:              &contest.Deleted,
+	})
 }

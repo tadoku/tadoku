807a627709bdaf269f1c4f8d3e870bbb1a310ae6
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2023-01-18 23:17:35 +0900

    webv2: add contest list

--- frontend/apps/webv2/pages/logs/[id].tsx
+++ frontend/apps/webv2/pages/logs/[id].tsx
@@ -5,6 +5,7 @@ import { HomeIcon } from '@heroicons/react/20/solid'
 import { XMarkIcon } from '@heroicons/react/24/outline'
 import { CalculatorIcon } from '@heroicons/react/24/solid'
 import { DateTime } from 'luxon'
+import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { Breadcrumb, chartColors } from 'ui'
 
@@ -45,58 +46,79 @@ const Page = () => {
           DateTime.DATETIME_MED,
         )}
       </h2>
-      <div className="w-fit">
-        <div className="card mt-4 w-full relative">
-          <div
-            className={`bg-${logColor}-300 absolute top-0 left-0 right-0 h-2`}
-          ></div>
-          <div className="h-stack spaced">
-            <span
-              className={`py-1 px-3 text-xs items-center flex bg-${logColor}-300 text-${logColor}-900`}
-            >
-              {log.data.activity.name}
-            </span>
-            <span className="py-1 px-3 text-xs items-center flex text-white bg-slate-500">
-              {log.data.language.name}
-            </span>
-            {log.data.tags.map(it => (
+      <div className="h-stack space-x-4 mt-4">
+        <div className="w-fit">
+          <div className="card w-full relative">
+            <div
+              className={`bg-${logColor}-300 absolute top-0 left-0 right-0 h-2`}
+            ></div>
+            <div className="h-stack spaced">
               <span
-                className={`py-1 px-3 text-xs items-center flex text-white bg-secondary`}
+                className={`py-1 px-3 text-xs items-center flex bg-${logColor}-300 text-${logColor}-900`}
               >
-                {it}
+                {log.data.activity.name}
               </span>
-            ))}
+              <span className="py-1 px-3 text-xs items-center flex text-white bg-slate-500">
+                {log.data.language.name}
+              </span>
+              {log.data.tags.map(it => (
+                <span
+                  className={`py-1 px-3 text-xs items-center flex text-white bg-secondary`}
+                >
+                  {it}
+                </span>
+              ))}
+            </div>
+            {log.data.description ? (
+              <p className="">{log.data.description}</p>
+            ) : null}
           </div>
-          {log.data.description ? (
-            <p className="">{log.data.description}</p>
-          ) : null}
-        </div>
-        <div className="grid card mt-4 w-full ">
-          <h3 className="subtitle mb-2">Score calculation</h3>
-          <div className="grid grid-cols-3 gap-x-4 gap-y-2 w-full">
-            <div className="col-span-2 text-right font-bold text-3xl">
-              {log.data.amount}
-            </div>
-            <div className="flex items-center lowercase font-bold px-4 bg-slate-500 text-white">
-              {formatUnit(log.data.amount, log.data.unit_name)}
-            </div>
-            <div className="flex items-center">
-              <XMarkIcon className="w-6 h-6" />
-            </div>
-            <div className="text-right font-bold text-3xl">
-              {log.data.modifier}
-            </div>
-            <div className="flex items-center lowercase font-bold px-4 bg-slate-500 text-white">
-              Modifier
-            </div>
-            <div className="col-span-2 text-right border-t-2 border-slate-500/20 font-bold text-3xl mt-2 pt-2">
-              {log.data.score}
-            </div>
-            <div className="flex items-center lowercase font-bold px-4 bg-secondary text-white mt-4">
-              Score
+          <div className="grid card mt-4 w-full ">
+            <h3 className="subtitle mb-2">Score calculation</h3>
+            <div className="grid grid-cols-3 gap-x-4 gap-y-2 w-full">
+              <div className="col-span-2 text-right font-bold text-3xl">
+                {log.data.amount}
+              </div>
+              <div className="flex items-center lowercase font-bold px-4 bg-slate-500 text-white">
+                {formatUnit(log.data.amount, log.data.unit_name)}
+              </div>
+              <div className="flex items-center">
+                <XMarkIcon className="w-6 h-6" />
+              </div>
+              <div className="text-right font-bold text-3xl">
+                {log.data.modifier}
+              </div>
+              <div className="flex items-center lowercase font-bold px-4 bg-slate-500 text-white">
+                Modifier
+              </div>
+              <div className="col-span-2 text-right border-t-2 border-slate-500/20 font-bold text-3xl mt-2 pt-2">
+                {log.data.score}
+              </div>
+              <div className="flex items-center lowercase font-bold px-4 bg-secondary text-white mt-4">
+                Score
+              </div>
             </div>
           </div>
         </div>
+        {log.data.registrations ? (
+          <div>
+            <div className="card w-64 p-0">
+              <h3 className="subtitle px-7 py-4">Contests</h3>
+              <ul className="divide-y-2 divide-slate-500/5 border-t-2 border-slate-500/5">
+                {log.data.registrations.map(it => (
+                  <li>
+                    <Link
+                      href={routes.contestLeaderboard(it.contest_id)}
+                      className="reset px-7 py-2 flex justify-between items-center hover:bg-slate-500/5 font-bold"
+                    >
+                      {it.title}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        ) : null}
       </div>
     </>
   )

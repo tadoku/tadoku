723a1499affd8d5d802ea92f8a95a68e35b2a68d
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2023-01-18 20:02:38 +0900

    immersion: http implementation find log by id

--- services/immersion-api/http/rest/logs.go
+++ services/immersion-api/http/rest/logs.go
@@ -109,5 +109,47 @@ func (s *Server) LogGetConfigurations(ctx echo.Context) error {
 // Fetches a log by id
 // (GET /logs/{id})
 func (s *Server) LogFindByID(ctx echo.Context, id types.UUID) error {
-	return nil
+	log, err := s.logQueryService.FindLogByID(ctx.Request().Context(), &logquery.FindLogByIDRequest{
+		ID: id,
+	})
+	if err != nil {
+		if errors.Is(err, logquery.ErrNotFound) {
+			return ctx.NoContent(http.StatusNotFound)
+		}
+		ctx.Echo().Logger.Error("could not process request: ", err)
+		return ctx.NoContent(http.StatusInternalServerError)
+	}
+
+	refs := make([]openapi.ContestRegistrationReference, len(log.Registrations))
+	for i, it := range log.Registrations {
+		refs[i] = openapi.ContestRegistrationReference{
+			ContestId:      it.ContestID,
+			RegistrationId: it.RegistrationID,
+			Title:          it.Title,
+		}
+	}
+
+	res := openapi.Log{
+		Id: log.ID,
+		Activity: openapi.Activity{
+			Id:   int32(log.ActivityID),
+			Name: log.ActivityName,
+		},
+		Language: openapi.Language{
+			Code: log.LanguageCode,
+			Name: log.LanguageName,
+		},
+		Amount:        log.Amount,
+		Modifier:      log.Modifier,
+		Score:         log.Score,
+		Tags:          log.Tags,
+		UnitName:      log.UnitName,
+		UserId:        log.UserID,
+		CreatedAt:     log.CreatedAt,
+		Deleted:       log.Deleted,
+		Description:   log.Description,
+		Registrations: &refs,
+	}
+
+	return ctx.JSON(http.StatusOK, res)
 }

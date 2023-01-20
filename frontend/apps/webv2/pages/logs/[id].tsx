4b9666c0994288483fe69c5ca46ac8ab279f19b5
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2023-01-20 21:00:15 +0900

    webv2: change width log details

--- frontend/apps/webv2/pages/logs/[id].tsx
+++ frontend/apps/webv2/pages/logs/[id].tsx
@@ -45,64 +45,62 @@ const Page = () => {
           DateTime.DATETIME_MED,
         )}
       </h2>
-      <div className="h-stack space-x-4 mt-4">
-        <div className="w-fit">
-          <div className="card w-full relative">
-            <div
-              className={`bg-${logColor}-300 absolute top-0 left-0 right-0 h-2`}
-            ></div>
-            <div className="h-stack spaced">
+      <div className="max-w-2xl mt-4">
+        <div className="card w-full relative">
+          <div
+            className={`bg-${logColor}-300 absolute top-0 left-0 right-0 h-2`}
+          ></div>
+          <div className="h-stack spaced">
+            <span
+              className={`py-1 px-3 text-xs items-center flex bg-${logColor}-300 text-${logColor}-900`}
+            >
+              {log.data.activity.name}
+            </span>
+            <span className="py-1 px-3 text-xs items-center flex text-slate-900 bg-slate-200">
+              {log.data.language.name}
+            </span>
+            {log.data.tags.map(it => (
               <span
-                className={`py-1 px-3 text-xs items-center flex bg-${logColor}-300 text-${logColor}-900`}
+                className={`py-1 px-3 text-xs items-center flex text-slate-900 bg-slate-200`}
               >
-                {log.data.activity.name}
+                {it}
               </span>
-              <span className="py-1 px-3 text-xs items-center flex text-slate-900 bg-slate-200">
-                {log.data.language.name}
-              </span>
-              {log.data.tags.map(it => (
-                <span
-                  className={`py-1 px-3 text-xs items-center flex text-slate-900 bg-slate-200`}
+            ))}
+          </div>
+          {log.data.registrations && log.data.registrations.length >= 1 ? (
+            <p>
+              Submitted to{' '}
+              {formatArray(log.data.registrations, it => (
+                <Link
+                  href={routes.contestLeaderboard(it.contest_id)}
+                  className="font-bold"
                 >
-                  {it}
-                </span>
+                  {it.title}
+                </Link>
               ))}
+            </p>
+          ) : null}
+          {log.data.description ? (
+            <>
+              <h3 className="subtitle my-2">Description</h3>
+              <p className="">{log.data.description}</p>
+            </>
+          ) : null}
+          <div className="h-stack w-full mt-4 spaced">
+            <div className="w-1/2">
+              <h3 className="subtitle mb-2">Score</h3>
+              <div className="font-bold text-5xl">{log.data.score}</div>
             </div>
-            {log.data.registrations && log.data.registrations.length >= 1 ? (
-              <p>
-                Submitted to{' '}
-                {formatArray(log.data.registrations, it => (
-                  <Link
-                    href={routes.contestLeaderboard(it.contest_id)}
-                    className="font-bold"
-                  >
-                    {it.title}
-                  </Link>
-                ))}
-              </p>
-            ) : null}
-            {log.data.description ? (
-              <>
-                <h3 className="subtitle my-2">Description</h3>
-                <p className="">{log.data.description}</p>
-              </>
-            ) : null}
-            <div className="h-stack w-full mt-4">
-              <div className="w-1/2">
-                <h3 className="subtitle mb-2">Score</h3>
-                <div className="font-bold text-5xl">{log.data.score}</div>
-              </div>
-              <div className="w-1/2 flex flex-col items-end justify-end opacity-80">
-                <h4 className="subtitle text-sm">Breakdown</h4>
-                <div className="lowercase flex items-center space-x-1 text-sm">
-                  <strong className="text-lg">{log.data.amount}</strong>
-                  <span className="text-slate-500">
-                    {formatUnit(log.data.amount, log.data.unit_name)}
-                  </span>
-                  <XMarkIcon className="w-3 h-3 mx-2 text-secondary" />
-                  <strong className="text-lg">{log.data.modifier}</strong>
-                  <span className="text-slate-500">modifier</span>
-                </div>
+            <div className="w-1/2 flex flex-col items-end justify-end opacity-80">
+              <h4 className="subtitle text-sm">Breakdown</h4>
+              <div className="lowercase flex items-center space-x-1 text-sm">
+                <strong className="text-lg">{log.data.amount}</strong>
+                <span className="text-slate-500">
+                  {formatUnit(log.data.amount, log.data.unit_name)}
+                </span>
+                <XMarkIcon className="w-3 h-3 mx-2 text-secondary" />
+                <strong className="text-lg">{log.data.modifier}</strong>
+                <span className="text-slate-500">modifier</span>
               </div>
             </div>
           </div>

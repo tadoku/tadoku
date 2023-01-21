76f8fecad485a1faa8e50b0b563fce4eb6625ce5
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2023-01-21 16:53:13 +0900

    styleguide: rough implementration of the heatmap

--- frontend/apps/styleguide/components/HeatmapChart.tsx
+++ frontend/apps/styleguide/components/HeatmapChart.tsx
@@ -1,10 +1,42 @@
-import { Chart as ChartJS } from 'chart.js'
+import { Chart as ChartJS, registerables } from 'chart.js'
 import { Chart } from 'react-chartjs-2'
 import { MatrixElement, MatrixController } from 'chartjs-chart-matrix'
+import { color } from 'chart.js/helpers'
 import 'chartjs-adapter-luxon'
 import { faker } from '@faker-js/faker'
 
 ChartJS.register(MatrixElement, MatrixController)
+
+function isoDayOfWeek(dt: Date) {
+  let wd = dt.getDay() // 0..6, from sunday
+  wd = ((wd + 6) % 7) + 1 // 1..7 from monday
+  return '' + wd // string so it gets parsed
+}
+
+function startOfToday() {
+  const d = new Date()
+  return new Date(d.getFullYear(), d.getMonth(), d.getDate(), 0, 0, 0, 0)
+}
+
+function generateData() {
+  const data = []
+  const end = startOfToday()
+  let dt = new Date(new Date().setDate(end.getDate() - 365))
+  while (dt <= end) {
+    const iso = dt.toISOString().substr(0, 10)
+    data.push({
+      x: iso,
+      y: isoDayOfWeek(dt),
+      d: iso,
+      v: Math.random() * 50,
+    })
+    dt = new Date(dt.setDate(dt.getDate() + 1))
+  }
+  console.log(data)
+  return data
+}
+
+const data = generateData()
 
 function HeatmapChart() {
   return (
@@ -13,33 +45,102 @@ function HeatmapChart() {
       data={{
         datasets: [
           {
-            label: 'Basic matrix',
-            data: [
-              { x: 1, y: 1 },
-              { x: 2, y: 1 },
-              { x: 1, y: 2 },
-              { x: 2, y: 2 },
-            ],
+            label: 'My Matrix',
+            data,
+            backgroundColor(c) {
+              const value = c.dataset.data[c.dataIndex].v
+              const alpha = (10 + value) / 60
+              return color('green').alpha(alpha).rgbString()
+            },
+            borderColor(c) {
+              const value = c.dataset.data[c.dataIndex].v
+              const alpha = (10 + value) / 60
+              return color('green').alpha(alpha).darken(0.3).rgbString()
+            },
             borderWidth: 1,
-            borderColor: 'rgba(0,0,0,0.5)',
-            backgroundColor: 'rgba(200,200,0,0.3)',
-            width: ({ chart }) => (chart.chartArea || {}).width / 2 - 1,
-            height: ({ chart }) => (chart.chartArea || {}).height / 2 - 1,
+            hoverBackgroundColor: 'yellow',
+            hoverBorderColor: 'yellowgreen',
+            width(c) {
+              const a = c.chart.chartArea || {}
+              return (a.right - a.left) / 53 - 1
+            },
+            height(c) {
+              const a = c.chart.chartArea || {}
+              return (a.bottom - a.top) / 7 - 1
+            },
           },
         ],
       }}
       options={{
-        scales: {
-          x: {
+        aspectRatio: 5,
+        plugins: {
+          legend: {
             display: false,
-            min: 0.5,
-            max: 2.5,
-            offset: false,
           },
+          tooltip: {
+            displayColors: false,
+            callbacks: {
+              title() {
+                return ''
+              },
+              label(context) {
+                const v = context.dataset.data[context.dataIndex]
+                return ['d: ' + v.d, 'v: ' + v.v.toFixed(2)]
+              },
+            },
+          },
+        },
+        scales: {
           y: {
-            display: false,
-            min: 0.5,
-            max: 2.5,
+            type: 'time',
+            offset: true,
+            time: {
+              unit: 'day',
+              round: 'day',
+              parser: 'd',
+              displayFormats: {
+                day: 'ccc',
+              },
+            },
+            reverse: true,
+            position: 'right',
+            ticks: {
+              maxRotation: 0,
+              autoSkip: false,
+              padding: 1,
+              font: {
+                size: 9,
+              },
+            },
+            grid: {
+              display: false,
+              tickLength: 0,
+            },
+          },
+          x: {
+            type: 'time',
+            position: 'bottom',
+            offset: true,
+            time: {
+              unit: 'week',
+              round: 'week',
+              minUnit: 'month',
+
+              displayFormats: {
+                // week: 'MMMM',
+              },
+            },
+            ticks: {
+              maxRotation: 0,
+              autoSkip: false,
+              font: {
+                size: 9,
+              },
+            },
+            grid: {
+              display: false,
+              tickLength: 0,
+            },
           },
         },
       }}

30893b45eb46b84b9681ad87b454f716bbfc8a75
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2023-01-21 20:26:17 +0900

    styleguide: heatmap component base

--- frontend/apps/styleguide/components/HeatmapChart.tsx
+++ frontend/apps/styleguide/components/HeatmapChart.tsx
@@ -1,20 +1,9 @@
-import { Chart as ChartJS, registerables } from 'chart.js'
-import { Chart } from 'react-chartjs-2'
-import { MatrixElement, MatrixController } from 'chartjs-chart-matrix'
-import { color } from 'chart.js/helpers'
-import 'chartjs-adapter-luxon'
-import { faker } from '@faker-js/faker'
-
-ChartJS.register(MatrixElement, MatrixController)
+import { DateTime, Interval } from 'luxon'
 
 function isoDayOfWeek(dt: Date) {
   let wd = dt.getDay() // 0..6, from sunday
   wd = ((wd + 6) % 7) + 1 // 1..7 from monday
   return '' + wd // string so it gets parsed
-}
-
-function startOfToday() {
-  return
 }
 
 function generateData() {
@@ -31,118 +20,130 @@ function generateData() {
     })
     dt = new Date(dt.setDate(dt.getDate() + 1))
   }
-  console.log(data)
   return data
 }
 
 const data = generateData()
 
-function HeatmapChart() {
-  return (
-    <Chart
-      type="matrix"
-      data={{
-        datasets: [
-          {
-            label: 'My Matrix',
-            data,
-            backgroundColor(c) {
-              const value = c.dataset.data[c.dataIndex].v
-              const alpha = (10 + value) / 60
-              return color('green').alpha(alpha).rgbString()
-            },
-            borderColor(c) {
-              const value = c.dataset.data[c.dataIndex].v
-              const alpha = (10 + value) / 60
-              return color('green').alpha(alpha).darken(0.3).rgbString()
-            },
-            borderWidth: 1,
-            hoverBackgroundColor: 'yellow',
-            hoverBorderColor: 'yellowgreen',
-            width(c) {
-              const a = c.chart.chartArea || {}
-              return (a.right - a.left) / 53 - 1
-            },
-            height(c) {
-              const a = c.chart.chartArea || {}
-              return (a.bottom - a.top) / 7 - 1
-            },
-          },
-        ],
-      }}
-      options={{
-        aspectRatio: 5,
-        plugins: {
-          legend: {
-            display: false,
-          },
-          tooltip: {
-            displayColors: false,
-            callbacks: {
-              title() {
-                return ''
-              },
-              label(context) {
-                const v = context.dataset.data[context.dataIndex]
-                return ['d: ' + v.d, 'v: ' + v.v.toFixed(2)]
-              },
-            },
-          },
-        },
-        scales: {
-          y: {
-            type: 'time',
-            offset: true,
-            time: {
-              unit: 'day',
-              round: 'day',
-              parser: 'd',
-              displayFormats: {
-                day: 'ccc',
-              },
-            },
-            reverse: true,
-            position: 'right',
-            ticks: {
-              maxRotation: 0,
-              autoSkip: false,
-              padding: 1,
-              font: {
-                size: 9,
-              },
-            },
-            grid: {
-              display: false,
-              tickLength: 0,
-            },
-          },
-          x: {
-            type: 'time',
-            position: 'bottom',
-            offset: true,
-            time: {
-              unit: 'week',
-              round: 'week',
+interface Cell {
+  x: DateTime
+  y: number
+  value: number
+}
 
-              displayFormats: {
-                month: 'MMMM',
-              },
-            },
-            ticks: {
-              maxRotation: 0,
-              autoSkip: false,
-              font: {
-                size: 9,
-              },
-            },
-            grid: {
-              display: false,
-              tickLength: 0,
-            },
-          },
-        },
-      }}
-    />
+function HeatmapChart() {
+  const year = 2022
+  const start = DateTime.fromObject({ year, month: 1, day: 1 })
+  const end = DateTime.fromObject({ year, month: 12, day: 31 })
+
+  const dates = Interval.fromDateTimes(start, end.endOf('day'))
+    .splitBy({ day: 1 })
+    .map(it => it.start)
+
+  const weeks = start.weeksInWeekYear + 1
+  const cols: (Cell | undefined)[][] = Array.from(Array(weeks)).map(_ =>
+    Array.from(Array(7)),
+  )
+
+  let col = 0
+  for (const date of dates) {
+    cols[col][date.weekday - 1] = {
+      x: date,
+      y: date.weekday,
+      value: Math.random() * 100,
+    }
+
+    if (date.weekday === 7) {
+      col += 1
+    }
+  }
+
+  const colWidth = 10
+  const rowHeight = 10
+  const padding = 3
+  const offset = {
+    x: 30,
+    y: 15,
+  }
+
+  const weekdays = ['Mon', undefined, 'Wed', undefined, 'Fri', undefined, 'Sun']
+
+  let lastMonth = 0
+  const months = cols.map(rows => {
+    const shouldRender = rows
+      .filter(it => it !== undefined)
+      .some(cell => cell!.x.month > lastMonth)
+    if (!shouldRender) {
+      return undefined
+    }
+
+    lastMonth += 1
+    return DateTime.fromObject({ month: lastMonth }).toFormat('LLL')
+  })
+
+  return (
+    <svg
+      width={weeks * colWidth + (weeks - 1) * padding + offset.x}
+      height={7 * rowHeight + 6 * padding + offset.y}
+      className=""
+    >
+      {weekdays.map((label, row) => {
+        if (!label) {
+          return null
+        }
+
+        return (
+          <text
+            textAnchor="end"
+            x={offset.x - padding * 2}
+            y={offset.y + rowHeight * row + padding * row}
+            className=""
+            alignmentBaseline="hanging"
+            style={{ fontSize: 10 }}
+          >
+            {label}
+          </text>
+        )
+      })}
+      {months.map((label, col) => {
+        if (!label) {
+          return null
+        }
+
+        return (
+          <text
+            textAnchor="start"
+            x={offset.x + colWidth * col + padding * col}
+            y={0}
+            className=""
+            alignmentBaseline="hanging"
+            style={{ fontSize: 10 }}
+          >
+            {label}
+          </text>
+        )
+      })}
+      {cols.map((rows, col) =>
+        rows.map((cell, row) => {
+          if (!cell) {
+            return null
+          }
+          return (
+            <rect
+              width={colWidth}
+              height={rowHeight}
+              x={offset.x + colWidth * col + padding * col}
+              y={offset.y + rowHeight * row + padding * row}
+              fill={'transparent'}
+              className={`fill-teal-400`}
+              strokeWidth={0}
+            >
+              <title>{cell.x.toLocaleString(DateTime.DATE_FULL)}</title>
+            </rect>
+          )
+        }),
+      )}
+    </svg>
   )
 }
 

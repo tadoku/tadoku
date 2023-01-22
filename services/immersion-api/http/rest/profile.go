4405ba6f2285675981c11c94dcbd6de05e72be5d
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2023-01-22 20:30:36 +0900

    immersion: users.scores http

--- services/immersion-api/http/rest/profile.go
+++ services/immersion-api/http/rest/profile.go
@@ -66,5 +66,25 @@ func (s *Server) ProfileYearlyActivityByUserID(ctx echo.Context, userId types.UU
 // Fetches the scores of a user for a given year
 // (GET /users/{userId}/scores/{year})
 func (s *Server) ProfileYearlyScoresByUserID(ctx echo.Context, userId types.UUID, year int) error {
-	return nil
+	summary, err := s.profileQueryService.YearlyScoresForUser(ctx.Request().Context(), &profilequery.YearlyScoresForUserRequest{
+		UserID: userId,
+		Year:   year,
+	})
+	if err != nil {
+		ctx.Logger().Errorf("could not fetch scores: %w", err)
+		return ctx.NoContent(http.StatusInternalServerError)
+	}
+
+	scores := make([]openapi.Score, len(summary.Scores))
+	for i, it := range summary.Scores {
+		scores[i] = openapi.Score{
+			LanguageCode: it.LanguageCode,
+			Score:        it.Score,
+		}
+	}
+
+	return ctx.JSON(http.StatusOK, &openapi.ProfileScores{
+		OverallScore: summary.OverallScore,
+		Scores:       scores,
+	})
 }

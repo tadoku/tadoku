7380334d79aa315f04a5fae762ef6c21d23c9b16
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2023-02-19 18:51:38 +0900

    webv2: fix default endpoint for prod

--- frontend/apps/webv2/next.config.js
+++ frontend/apps/webv2/next.config.js
@@ -5,11 +5,17 @@ const nextConfig = {
   swcMinify: true,
   publicRuntimeConfig: {
     // TODO: Figure out why this isn't getting passed to the client despite being opted-out from automatic static optimization
-    kratosPublicEndpoint: process.env.NEXT_PUBLIC_KRATOS_ENDPOINT ?? 'https://account.tadoku.app/kratos',
-    kratosInternalEndpoint: process.env.NEXT_PUBLIC_KRATOS_INTERNAL_ENDPOINT ?? 'http://kratos-public',
-    authUiUrl: process.env.NEXT_PUBLIC_AUTH_UI_URL ?? 'https://account.tadoku.app',
-    homeUrl: process.env.NEXT_PUBLIC_HOME_URL ?? 'https://staging.tadoku.app',
-    apiEndpoint: process.env.NEXT_PUBLIC_API_ENDPOINT ?? 'https://staging.tadoku.app/api/internal',
+    kratosPublicEndpoint:
+      process.env.NEXT_PUBLIC_KRATOS_ENDPOINT ??
+      'https://account.tadoku.app/kratos',
+    kratosInternalEndpoint:
+      process.env.NEXT_PUBLIC_KRATOS_INTERNAL_ENDPOINT ??
+      'http://kratos-public',
+    authUiUrl:
+      process.env.NEXT_PUBLIC_AUTH_UI_URL ?? 'https://account.tadoku.app',
+    homeUrl: process.env.NEXT_PUBLIC_HOME_URL ?? 'https://tadoku.app',
+    apiEndpoint:
+      process.env.NEXT_PUBLIC_API_ENDPOINT ?? 'https://tadoku.app/api/internal',
     cookieDomain: process.env.NEXT_PUBLIC_COOKIE_DOMAIN ?? '.tadoku.app',
   },
   transpilePackages: ['ui'],

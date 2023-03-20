0f3f466b23f901831d071b84480fe7453b510d9e
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2023-03-20 16:09:18 +0900

    immersion: backfill user display names

--- services/immersion-api/storage/postgres/migrations/0002_init_users.up.sql
+++ services/immersion-api/storage/postgres/migrations/0002_init_users.up.sql
@@ -8,4 +8,14 @@ create table users (
   updated_at timestamp not null default now()
 );
 
+-- backfill data based on existing registrations
+insert into users (id, display_name)
+(
+  select
+    user_id,
+    (select user_display_name from contest_registrations where user_id = r.user_id order by created_at desc limit 1)
+  from contest_registrations as r
+  group by user_id
+);
+
 commit;

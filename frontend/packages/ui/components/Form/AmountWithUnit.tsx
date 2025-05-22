715a6d2ea3989f5182e6b55d316a4aedd6906270
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2025-05-23 00:00:00 +0900

    style amountwithunit

--- frontend/packages/ui/components/Form/AmountWithUnit.tsx
+++ frontend/packages/ui/components/Form/AmountWithUnit.tsx
@@ -36,15 +36,14 @@ export function AmountWithUnit<T extends FieldValues>(
       <div className="flex h-11 overflow-visible input-frame group focus-within:ring-2 focus-within:ring-primary/40 focus-within:border-primary transition ease-in-out">
         <input
           type="number"
-          step="any"
           {...inputProps}
           id={`${name}.value`}
           {...register(`${name}.value`, { valueAsNumber: true })}
-          className="!border-l-0 !border-t-0 !border-b-0 !border-r border-black/10 focus:!border-black/10 !h-10 !bg-none focus:!ring-0 focus:!outline-none w-full"
+          className="!border-l-0 !border-t-0 !border-b-0 !border-r border-black/10 focus:!border-black/10 !h-full !bg-none focus:!ring-0 focus:!outline-none w-full"
         />
         <select
           {...register(`${name}.unit`)}
-          className="w-auto !h-10 px-2 pr-8 border-none focus:!ring-0 focus:!outline-none bg-transparent"
+          className="w-auto !h-full px-2 pr-8 border-none bg-black/5 focus:!ring-0 focus:!outline-none focus:bg-transparent"
         >
           {units.map(unit => (
             <option key={unit.value} value={unit.value}>

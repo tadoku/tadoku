d73ab56ab8cb392670e10e111888f90c2a907e5d
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2025-10-06 00:00:00 +0900

    update styleguide to include table with action button example

--- frontend/apps/styleguide/pages/tables.tsx
+++ frontend/apps/styleguide/pages/tables.tsx
@@ -1,4 +1,4 @@
-import { CodeBlock, Preview, Title } from '@components/example'
+import { CodeBlock, Preview, Separator, Title } from '@components/example'
 import {
   createColumnHelper,
   flexRender,
@@ -6,7 +6,8 @@ import {
   useReactTable,
   RowData,
 } from '@tanstack/react-table'
-
+import { ActionMenu } from 'ui'
+import { EllipsisVerticalIcon } from '@heroicons/react/20/solid'
 export default function Page() {
   return (
     <>
@@ -135,6 +136,143 @@ function ExampleTable() {
   )
 }`}
       />
+
+      <Separator />
+
+      <Title>Clickable Rows with ActionMenu</Title>
+      <Preview>
+        <TableWithActionMenu />
+      </Preview>
+      <CodeBlock
+        code={`import { ActionMenu } from 'ui'
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+  RowData,
+} from '@tanstack/react-table'
+import { EllipsisVerticalIcon } from '@heroicons/react/20/solid'
+
+const data: Ranking[] = [
+  { rank: '1', user: 'powz', score: 5054.25054 },
+  { rank: '2', user: 'Bijak', score: 3605.23605 },
+  { rank: '3', user: 'ShockOLatte', score: 2518.72518 },
+  { rank: '4', user: 'Ludie', score: 2517.32517 },
+  { rank: '5', user: 'Chamsae', score: 2434.42434 },
+]
+
+interface Ranking {
+  rank: string
+  user: string
+  score: number
+}
+
+const columnHelper = createColumnHelper<Ranking>()
+
+const columns = [
+  columnHelper.accessor('rank', {
+    header: () => 'Rank',
+    size: 5,
+    meta: {
+      className: 'justify-center',
+    },
+  }),
+  columnHelper.accessor('user', {
+    header: () => 'User',
+  }),
+  columnHelper.accessor('score', {
+    header: () => 'Score',
+    size: 20,
+    cell: info => Math.round(info.getValue()),
+    meta: {
+      className: 'justify-end text-right',
+    },
+  }),
+  columnHelper.display({
+    id: 'actions',
+    header: () => 'Actions',
+    size: 10,
+    cell: () => (
+      <ActionMenu
+        orientation="right"
+        links={[
+          { label: 'Edit', href: '#edit', type: 'normal' },
+          { label: 'Delete', href: '#delete', type: 'danger' },
+        ]}
+      >
+        <EllipsisVerticalIcon className="w-4 h-5" />
+      </ActionMenu>
+    ),
+  }),
+]
+
+function TableWithActionMenu() {
+  const table = useReactTable({
+    defaultColumn: {
+      minSize: 0,
+      size: 0,
+    },
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  })
+
+  return (
+    <table className="default w-full border-collapse">
+      <thead>
+        {table.getHeaderGroups().map(headerGroup => (
+          <tr key={headerGroup.id}>
+            {headerGroup.headers.map(header => (
+              <th
+                key={header.id}
+                style={{
+                  width: header.getSize() !== 0 ? header.getSize() : undefined,
+                }}
+                className={\`default \${header.column.columnDef.meta?.className}\`}
+              >
+                {header.isPlaceholder
+                  ? null
+                  : flexRender(
+                      header.column.columnDef.header,
+                      header.getContext(),
+                    )}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody>
+        {table.getRowModel().rows.map(row => (
+          <tr key={row.id} className="link">
+            {row.getVisibleCells().map(cell => {
+              const isActionColumn = cell.column.id === 'actions'
+              return (
+                <td key={cell.id} className="font-bold link">
+                  {isActionColumn ? (
+                    <div className="h-14 px-4 flex items-center justify-end">
+                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                    </div>
+                  ) : (
+                    <a
+                      href={\`#\${row.renderValue('rank')}\`}
+                      className={\`reset \${
+                        cell.column.columnDef.meta?.className
+                      }\`}
+                    >
+                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                    </a>
+                  )}
+                </td>
+              )
+            })}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
+}`}
+      />
     </>
   )
 }
@@ -224,20 +362,124 @@ function ExampleTable() {
       </thead>
       <tbody>
         {table.getRowModel().rows.map(row => (
-          <tr
-            key={row.id}
-            className="font-bold text-xl hover:bg-primary hover:text-white h-14"
-          >
+          <tr key={row.id} className="font-bold link">
             {row.getVisibleCells().map(cell => (
-              <td key={cell.id} className="border-0 p-0">
+              <td key={cell.id} className="link">
                 <a
                   href={`#${row.renderValue('rank')}`}
-                  className={`reset h-14 px-4 flex items-center ${cell.column.columnDef.meta?.className}`}
+                  className={`reset ${cell.column.columnDef.meta?.className}`}
                 >
                   {flexRender(cell.column.columnDef.cell, cell.getContext())}
                 </a>
               </td>
             ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
+}
+
+const columnsWithActions = [
+  columnHelper.accessor('rank', {
+    header: () => 'Rank',
+    size: 5,
+    meta: {
+      className: 'justify-center',
+    },
+  }),
+  columnHelper.accessor('user', {
+    header: () => 'User',
+  }),
+  columnHelper.accessor('score', {
+    header: () => 'Score',
+    size: 20,
+    cell: info => Math.round(info.getValue()),
+    meta: {
+      className: 'justify-end text-right',
+    },
+  }),
+  columnHelper.display({
+    id: 'actions',
+    header: () => 'Actions',
+    size: 10,
+    cell: () => (
+      <ActionMenu
+        orientation="right"
+        links={[
+          { label: 'Edit', href: '#edit', type: 'normal' },
+          { label: 'Delete', href: '#delete', type: 'danger' },
+        ]}
+      >
+        <EllipsisVerticalIcon className="w-4 h-5" />
+      </ActionMenu>
+    ),
+  }),
+]
+
+function TableWithActionMenu() {
+  const table = useReactTable({
+    defaultColumn: {
+      minSize: 0,
+      size: 0,
+    },
+    data,
+    columns: columnsWithActions,
+    getCoreRowModel: getCoreRowModel(),
+  })
+
+  return (
+    <table className="default w-full border-collapse">
+      <thead>
+        {table.getHeaderGroups().map(headerGroup => (
+          <tr key={headerGroup.id}>
+            {headerGroup.headers.map(header => (
+              <th
+                key={header.id}
+                style={{
+                  width: header.getSize() !== 0 ? header.getSize() : undefined,
+                }}
+                className={`default ${header.column.columnDef.meta?.className}`}
+              >
+                {header.isPlaceholder
+                  ? null
+                  : flexRender(
+                      header.column.columnDef.header,
+                      header.getContext(),
+                    )}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody>
+        {table.getRowModel().rows.map(row => (
+          <tr key={row.id} className="link">
+            {row.getVisibleCells().map(cell => {
+              const isActionColumn = cell.column.id === 'actions'
+              return (
+                <td key={cell.id} className="font-bold link">
+                  {isActionColumn ? (
+                    <div className="h-14 px-4 flex items-center justify-end">
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext(),
+                      )}
+                    </div>
+                  ) : (
+                    <a
+                      href={`#${row.renderValue('rank')}`}
+                      className={`reset ${cell.column.columnDef.meta?.className}`}
+                    >
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext(),
+                      )}
+                    </a>
+                  )}
+                </td>
+              )
+            })}
           </tr>
         ))}
       </tbody>

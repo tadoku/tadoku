3f98ea849cd76e8b61a84841ecfddfff1084be62
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2026-02-05 04:25:31 +0900

    Remove legacy ParseSession helper

--- services/common/domain/session.go
+++ services/common/domain/session.go
@@ -1,11 +1,4 @@
 package domain
 
-import "context"
-
 // Deprecated: use UserIdentity instead. This alias exists for migration.
 type SessionToken = UserIdentity
-
-// ParseSession is deprecated, use ParseUserIdentity instead.
-func ParseSession(ctx context.Context) *SessionToken {
-	return ParseUserIdentity(ctx)
-}
